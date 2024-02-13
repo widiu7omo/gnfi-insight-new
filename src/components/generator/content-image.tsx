0208cfb0262e6de551c80dcb4227dfe0dba0664b
Danar Widi <widiu7omo@gmail.com>
2024-02-13 15:36:02 +0800

    improvement: working preview image upload

--- src/components/generator/content-image.tsx
+++ src/components/generator/content-image.tsx
@@ -1,5 +1,10 @@
 import { ImageIcon } from "lucide-react";
+import { useCallback, useState } from "react";
+import { useDropzone } from "react-dropzone";
 
+type FileExtended = File & {
+  preview: string;
+};
 export default function ContentImage({ preview }: { preview?: boolean }) {
   if (preview) {
     return (
@@ -9,10 +14,55 @@ export default function ContentImage({ preview }: { preview?: boolean }) {
       </div>
     );
   }
+  const [files, setFiles] = useState<FileExtended[]>([]);
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    // Do something with the files
+    console.log(acceptedFiles);
+    console.log(typeof acceptedFiles);
+    setFiles(
+      acceptedFiles.map(
+        (file) =>
+          Object.assign(file, {
+            preview: URL.createObjectURL(file),
+          }) as FileExtended
+      )
+    );
+  }, []);
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop,
+    accept: {
+      "image/*": [],
+    },
+  });
   return (
-    <div className="border-2 border-dashed rounded-xl h-[100px] flex items-center justify-center flex-col space-y-2">
-      <ImageIcon className="text-neutral-500" size={32} />
-      <div className="text-neutral-500">Drop your image here</div>
+    <div {...getRootProps()}>
+      <div
+        className={`rounded-xl ${
+          files.length > 0 ? "h-full" : "h-[100px]"
+        } flex items-center justify-center flex-col space-y-2`}
+      >
+        {files.length > 0 ? (
+          <img
+            className="h-full w-auto rounded-xl"
+            src={files.at(0)?.preview}
+            alt={files.at(0)?.name}
+          />
+        ) : (
+          <div className="flex items-center justify-center flex-col">
+            <ImageIcon className="text-neutral-500" size={32} strokeWidth={1} />
+            <input {...getInputProps()} />
+            {isDragActive ? (
+              <p className="text-neutral-500 text-sm">
+                Drop the files here ...
+              </p>
+            ) : (
+              <p className="text-neutral-500 text-sm">
+                Drag 'n' drop some files here, or click to select files
+              </p>
+            )}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
--- src/components/generator/content-renderer.tsx
+++ src/components/generator/content-renderer.tsx
@@ -1,7 +1,13 @@
-import { COMPONENT_CONTENT, COMPONENT_HEADER } from "@/data/component-front";
+import {
+  COMPONENT_CONTENT,
+  COMPONENT_HEADER,
+  COMPONENT_HERO,
+  COMPONENT_IMAGE,
+} from "@/data/component-front";
 import { BlockType } from "@/data/types";
 import ContentHeading from "./content-heading";
 import ContentParagraph from "./content-paragraph";
+import ContentImage from "./content-image";
 
 export default function ContentRenderer({ block }: { block: BlockType }) {
   if (block.component === COMPONENT_HEADER) {
@@ -9,6 +15,9 @@ export default function ContentRenderer({ block }: { block: BlockType }) {
   }
   if (block.component === COMPONENT_CONTENT) {
     return <ContentParagraph />;
+  }
+  if (block.component === COMPONENT_HERO) {
+    return <ContentImage />;
   }
   return <div>{JSON.stringify(block)}</div>;
 }
