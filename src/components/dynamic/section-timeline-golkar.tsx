13cfcbde507d79e6b9254140e4fc8f1bc0db7e66
Danar Widi <widiu7omo@gmail.com>
2024-02-19 22:49:54 +0800

    responsive timeline golkar

--- src/components/dynamic/section-timeline-golkar.tsx
+++ src/components/dynamic/section-timeline-golkar.tsx
@@ -21,10 +21,10 @@ export default function SectionTimelineGolkar() {
 
   return (
     <>
-      <div className="flex flex-row items-start justify-center w-full h-[600px] xl:h-[800px]">
+      <div className="flex flex-row items-start justify-center w-full h-[500px] xs:h-[700px] sm:h-[900px] md:h-[1000px] lg:h-[1200px] xl:h-[1200px]">
         <motion.div
           viewport={{ once: true }}
-          className="relative w-full h-[635px]"
+          className="relative w-full h-full max-w-7xl mx-auto flex justify-center"
           initial="hidden"
           whileInView="visible"
           variants={containerAnimate}
@@ -35,7 +35,7 @@ export default function SectionTimelineGolkar() {
             variants={item}
             initial={{ scaleY: 0 }}
             whileInView={{ scaleY: 1 }}
-            className="absolute z-10 left-[49.50%] lg:left-[49.76%] h-full top-[8%] lg:top-[230px] w-[2px] bg-black"
+            className="absolute z-10 left-[49.63%] h-[calc(100%-100px)] xs:h-[calc(100%-100px)] md:h-[calc(100%-200px)] xl:h-[calc(100%-200px)] top-[20px] xs:top-[100px] lg:top-[200px] w-[2px] bg-black"
           />
           {new Array(8).fill(0).map((_, i) => {
             indexImage++;
@@ -49,7 +49,7 @@ export default function SectionTimelineGolkar() {
                 }`}
                 className={`absolute ${
                   indexImage === 3 ? "z-30" : "z-20"
-                } xl:w-[50%] left-0 xl:left-[25%] -top-[230px] `}
+                } -top-[100px] xl:-top-[300px] max-w-full xl:max-w-7xl xl:mx-auto`}
                 src={`pemilu-dan-dinamikanya-dari-masa-ke-masa/assets/golkar/${indexImage}.png`}
                 alt={indexImage.toString()}
               />
