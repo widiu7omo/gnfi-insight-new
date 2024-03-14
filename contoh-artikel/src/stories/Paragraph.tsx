a4056f0a1237e9cc3a1cc34d37da15858b04be20
Danar Widi <widiu7omo@gmail.com>
2024-03-14 12:28:09 +0800

    add new component paragraph

---
+++ contoh-artikel/src/stories/Paragraph.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { stagger, useAnimate, useInView } from "framer-motion";
+import { type PropsWithChildren, useEffect } from "react";
+type ParagraphType = {
+	className?: string;
+};
+export function Paragraph({
+	children,
+	className,
+}: PropsWithChildren<ParagraphType>) {
+	const [scope, animate] = useAnimate();
+	const isInView = useInView(scope, { once: true });
+	useEffect(() => {
+		if (isInView) {
+			animate(
+				"p",
+				{ opacity: 1, y: 0 },
+				{ delay: stagger(0.1, { startDelay: 0.15 }), duration: 0.3 },
+			);
+		} else {
+			animate("p", { opacity: 0, y: 20 });
+		}
+	}, [isInView, animate]);
+
+	return (
+		<div
+			className={cn("prose relative z-[5] mx-auto px-4 xl:px-0", className)}
+			ref={scope}
+		>
+			{children}
+		</div>
+	);
+}
--- contoh-artikel/src/stories/SectionHeader.stories.tsx
+++ contoh-artikel/src/stories/SectionHeader.stories.tsx
@@ -20,14 +20,16 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
+export const Default: Story = {};
+
 export const CustomTitle: Story = {
 	args: {
 		customTitle: (
-			<h1 className="text-7xl font-bold text-white px-4 text-center leading-snug">
-				Membangun IKN, Mewujudkan Wacana Lama Indonesia
-			</h1>
+			<div className="mx-4 max-w-full rounded-xl border-2 border-white/10 bg-white/30 px-10 py-8 text-center backdrop-blur-sm xl:mx-0 xl:max-w-5xl">
+				<h2 className="text-3xl font-bold text-white drop-shadow-lg xl:text-5xl">
+					Mewujudkan Wacana Lama
+				</h2>
+			</div>
 		),
 	},
 };
-
-export const LoggedOut: Story = {};
