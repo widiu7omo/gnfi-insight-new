4750459c252bc9a2e2425d51387868fa2f4ea2e2
Danar Widi <widiu7omo@gmail.com>
2024-05-20 11:23:42 +0800

    fix: story component

--- contoh-artikel/src/stories/SectionHeader.stories.tsx
+++ contoh-artikel/src/stories/SectionHeader.stories.tsx
@@ -1,5 +1,4 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { fn } from "@storybook/test";
 import { SectionHeader } from "./SectionHeader";
 import { COMPONENT_SECTION_HEADER } from "@/data/component-front";
 
@@ -33,11 +32,11 @@ export const CustomTitle: Story = {
 		block: {
 			componentProps: {
 				customTitle: (
-					<div className="mx-4 max-w-full rounded-xl border-2 border-white/10 bg-white/30 px-10 py-8 text-center backdrop-blur-sm xl:mx-0 xl:max-w-5xl">
+					`<div className="mx-4 max-w-full rounded-xl border-2 border-white/10 bg-white/30 px-10 py-8 text-center backdrop-blur-sm xl:mx-0 xl:max-w-5xl">
 						<h2 className="text-3xl font-bold text-white drop-shadow-lg xl:text-5xl">
 							Mewujudkan Wacana Lama
 						</h2>
-					</div>
+					</div>`
 				),
 				coverImage: "/assets/section-header-example.webp",
 			},
