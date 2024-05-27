1fc5e27a01cd3119f1d8e98ec23605bed5e9b581
Danar Widi <widiu7omo@gmail.com>
2024-05-27 15:50:09 +0800

    ignore eslint

--- cerita-dari-megahnya-masjid-istiqlal/next.config.mjs
+++ cerita-dari-megahnya-masjid-istiqlal/next.config.mjs
@@ -2,7 +2,12 @@
 const nextConfig = {
   output: 'export',
   transpilePackages: ['lucide-react'],
-  images: { unoptimized: true }
+  images: { unoptimized: true },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 }
 
 export default nextConfig
