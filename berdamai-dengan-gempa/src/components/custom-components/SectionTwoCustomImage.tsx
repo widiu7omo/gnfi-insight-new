5b40c9ac5f52640828f207ebce4b56fdf2288299
Danar Widi <widiu7omo@gmail.com>
2024-06-25 17:33:52 +0800

    new-insight: gempa section 3

--- berdamai-dengan-gempa/src/components/custom-components/SectionTwoCustomImage.tsx
+++ berdamai-dengan-gempa/src/components/custom-components/SectionTwoCustomImage.tsx
@@ -83,7 +83,7 @@ export function SectionTwoCustomImage() {
             </div>
 
         </div>
-        <div className="max-w-4xl mx-auto pt-0 z-20">
+        <div className="max-w-4xl mx-auto pt-0 z-20 relative">
             <motion.img
                 initial={{ opacity: 0, x: 30 }}
                 viewport={{ once: true }}
@@ -103,5 +103,17 @@ export function SectionTwoCustomImage() {
                 transition={{ delay: 0.2 }}
                 src={`${baseUrl}/assets/2.15.png`} alt="GNFI Insight" />
         </div>
+        <div className="absolute left-0 z-0 right-0 bottom-0 top-[40%] bg-gradient-to-b from-black/0 to-black" />
+        <Paragraph block={{
+            component: COMPONENT_PARAGRAPH,
+            componentProps: {
+                className: "text-white",
+                children: (<>
+                    <p>Selain Cincin Api Pasifik, berbagai daerah di Indonesia sangat rawan bencana gempa bumi dan tsunami karena wilayah Indonesia dikelilingi oleh lempeng eurasia, lempeng Indo-Australia dan juga Lempeng Pasifik.</p>
+                    <p>“Setiap saat lempeng ini akan bergeser dan bergerak menimbulkan gempa bumi. Gerakan gempa yang terjadi di dasar laut dapat mengguncangkan massa air laut dan menimbulkan terjadinya tsunami,” tulis Dadang Sungkawa dalam jurnal Gea.</p>
+                    <p>Menurut Direktorat Vulkanologi dan Mitigasi Bencana Geologi (DVMBG) Departemen Energi dan Sumber Daya Mineral tercatat ada 28 wilayah di Indonesia yang dinyatakan rawan gempa bumi dan tsunami. Ke-28 daerah tersebut tersebar di berbagai penjuru, mulai dari Aceh dan Sumatra Utara di ujung barat Indonesia, hingga Yapen dan Fak-Fak di ujung timur Indonesia.</p>
+                </>)
+            }
+        }} />
     </div>
 }
