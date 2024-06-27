76d3c9956a38e245c9f9f9013df505646883e37a
Danar Widi <widiu7omo@gmail.com>
2024-06-27 11:48:46 +0800

    new-section: gempa, 4,5

--- berdamai-dengan-gempa/src/components/custom-components/SectionFiveCustomImage.tsx
+++ berdamai-dengan-gempa/src/components/custom-components/SectionFiveCustomImage.tsx
@@ -1,64 +1,57 @@
 "use client"
-import { baseUrl } from "@/constants/meta";
-import { motion } from "framer-motion";
+
+import { baseUrl } from "@/constants/meta"
+import { motion } from "framer-motion"
 
 export function SectionFiveCustomImage() {
-    return <div className="bg-cover relative xl:bg-[center_top_-30rem]" style={{ backgroundImage: `url(${baseUrl}/assets/3.1.png)` }}>
-        <div className="absolute bg-gradient-to-t from-[#04070B] z-0 left-0 right-0 top-[20%] bottom-0" />
-        <div className="max-w-4xl mx-auto pt-0 xl:pt-[14rem] z-20 relative">
+    return <div className="bg-cover relative xl:bg-[center_top_-30rem]" style={{ backgroundImage: `url(${baseUrl}/assets/5.1.png)` }}>
+        <div className="max-w-4xl mx-auto pt-0 xl:pt-6 z-20 relative ">
             <motion.img
                 initial={{ opacity: 0, y: 20 }}
                 viewport={{ once: true }}
                 whileInView={{ opacity: 1, y: 0 }}
                 transition={{ delay: 0.1 }}
-                src={`${baseUrl}/assets/3.2.png`} alt="Title" />
-            <div className="relative flex flex-col justify-end">
-                <motion.img
-                    initial={{ opacity: 0, x: -30 }}
-                    viewport={{ once: true }}
-                    whileInView={{ opacity: 1, x: 0 }}
-                    transition={{ delay: 0.2 }}
-                    src={`${baseUrl}/assets/3.3.png`} alt="Who memperkirakan" className="" />
-                <motion.img
-                    initial={{ opacity: 0, x: 30 }}
-                    viewport={{ once: true }}
-                    whileInView={{ opacity: 1, x: 0 }}
-                    transition={{ delay: 0.4 }}
-                    src={`${baseUrl}/assets/3.4.png`} alt="Who memperkirakan" className="absolute" />
-            </div>
-            <motion.img
-                initial={{ opacity: 0, y: 20 }}
-                viewport={{ once: true }}
-                whileInView={{ opacity: 1, y: 0 }}
-                transition={{ delay: 0.1 }}
-                src={`${baseUrl}/assets/3.5.png`} alt="Who memperkirakan" className="" />
-            <motion.img
-                initial={{ opacity: 0, x: 30 }}
-                viewport={{ once: true }}
-                whileInView={{ opacity: 1, x: 0 }}
-                transition={{ delay: 0.2 }}
-                src={`${baseUrl}/assets/3.6.png`} alt="Who memperkirakan" className="pt-0 xl:pt-[10rem]" />
-            <div className="relative flex flex-col justify-end">
-                <motion.img
-                    initial={{ opacity: 0, x: -30 }}
-                    viewport={{ once: true }}
-                    whileInView={{ opacity: 1, x: 0 }}
-                    transition={{ delay: 0.4 }}
-                    src={`${baseUrl}/assets/3.7.png`} alt="Who memperkirakan" className="absolute" />
-                <motion.img
-                    initial={{ opacity: 0, x: 30 }}
-                    viewport={{ once: true }}
-                    whileInView={{ opacity: 1, x: 0 }}
-                    transition={{ delay: 0.6 }}
-                    src={`${baseUrl}/assets/3.8.png`} alt="Who memperkirakan" className="" />
-                <motion.img
-                    initial={{ opacity: 0, x: -30 }}
-                    viewport={{ once: true }}
-                    whileInView={{ opacity: 1, x: 0 }}
-                    transition={{ delay: 0.8 }}
-                    src={`${baseUrl}/assets/3.9.png`} alt="Who memperkirakan" className="absolute bottom-0" />
-            </div>
+                src={`${baseUrl}/assets/5.2.png`} alt="Illustration" />
 
+        </div>
+        <div className="" style={{ background: `url(${baseUrl}/assets/4.1.1.png),white` }}>
+            <div className="max-w-4xl mx-auto pt-0 xl:pt-6 z-20 relative]">
+                <div className="relative flex flex-col justify-end">
+                    <motion.img
+                        initial={{ opacity: 0, x: -30 }}
+                        viewport={{ once: true }}
+                        whileInView={{ opacity: 1, x: 0 }}
+                        transition={{ delay: 0.4 }}
+                        src={`${baseUrl}/assets/5.3.png`} alt="Title" className="-mt-[7rem] sm:-mt-[8rem] xl:-mt-[15rem] z-10" />
+                    <motion.img
+                        initial={{ opacity: 0, x: 30 }}
+                        viewport={{ once: true }}
+                        whileInView={{ opacity: 1, x: 0 }}
+                        transition={{ delay: 0.2 }}
+                        src={`${baseUrl}/assets/5.4.png`} alt="Thinking" className="absolute bottom-0" />
+
+                </div>
+                <div className="relative flex flex-col justify-end ">
+                    <motion.img
+                        initial={{ opacity: 0, y: 20 }}
+                        viewport={{ once: true }}
+                        whileInView={{ opacity: 1, y: 0 }}
+                        transition={{ delay: 0.5 }}
+                        src={`${baseUrl}/assets/5.5.png`} alt="Card 1" className="mix-blend-darken" />
+                    <motion.img
+                        initial={{ opacity: 0, y: 20 }}
+                        viewport={{ once: true }}
+                        whileInView={{ opacity: 1, y: 0 }}
+                        transition={{ delay: 0.5 }}
+                        src={`${baseUrl}/assets/5.6.png`} alt="Card 1" className="absolute" />
+                    <motion.img
+                        initial={{ opacity: 0, y: 20 }}
+                        viewport={{ once: true }}
+                        whileInView={{ opacity: 1, y: 0 }}
+                        transition={{ delay: 0.1 }}
+                        src={`${baseUrl}/assets/5.7.png`} alt="Card 2" className="absolute z-20 " />
+                </div>
+            </div>
         </div>
     </div>
 }
---
+++ berdamai-dengan-gempa/src/components/custom-components/SectionThreeCustomImage.tsx
@@ -0,0 +1,64 @@
+"use client"
+import { baseUrl } from "@/constants/meta";
+import { motion } from "framer-motion";
+
+export function SectionThreeCustomImage() {
+    return <div className="bg-cover relative xl:bg-[center_top_-30rem]" style={{ backgroundImage: `url(${baseUrl}/assets/3.1.png)` }}>
+        <div className="absolute bg-gradient-to-t from-[#04070B] z-0 left-0 right-0 top-[20%] bottom-0" />
+        <div className="max-w-4xl mx-auto pt-0 xl:pt-[14rem] z-20 relative">
+            <motion.img
+                initial={{ opacity: 0, y: 20 }}
+                viewport={{ once: true }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ delay: 0.1 }}
+                src={`${baseUrl}/assets/3.2.png`} alt="Title" />
+            <div className="relative flex flex-col justify-end">
+                <motion.img
+                    initial={{ opacity: 0, x: -30 }}
+                    viewport={{ once: true }}
+                    whileInView={{ opacity: 1, x: 0 }}
+                    transition={{ delay: 0.2 }}
+                    src={`${baseUrl}/assets/3.3.png`} alt="Who memperkirakan" className="" />
+                <motion.img
+                    initial={{ opacity: 0, x: 30 }}
+                    viewport={{ once: true }}
+                    whileInView={{ opacity: 1, x: 0 }}
+                    transition={{ delay: 0.4 }}
+                    src={`${baseUrl}/assets/3.4.png`} alt="Who memperkirakan" className="absolute" />
+            </div>
+            <motion.img
+                initial={{ opacity: 0, y: 20 }}
+                viewport={{ once: true }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ delay: 0.1 }}
+                src={`${baseUrl}/assets/3.5.png`} alt="Who memperkirakan" className="" />
+            <motion.img
+                initial={{ opacity: 0, x: 30 }}
+                viewport={{ once: true }}
+                whileInView={{ opacity: 1, x: 0 }}
+                transition={{ delay: 0.2 }}
+                src={`${baseUrl}/assets/3.6.png`} alt="Who memperkirakan" className="pt-0 xl:pt-[10rem]" />
+            <div className="relative flex flex-col justify-end">
+                <motion.img
+                    initial={{ opacity: 0, x: -30 }}
+                    viewport={{ once: true }}
+                    whileInView={{ opacity: 1, x: 0 }}
+                    transition={{ delay: 0.4 }}
+                    src={`${baseUrl}/assets/3.7.png`} alt="Who memperkirakan" className="absolute" />
+                <motion.img
+                    initial={{ opacity: 0, x: 30 }}
+                    viewport={{ once: true }}
+                    whileInView={{ opacity: 1, x: 0 }}
+                    transition={{ delay: 0.6 }}
+                    src={`${baseUrl}/assets/3.8.png`} alt="Who memperkirakan" className="" />
+                <motion.img
+                    initial={{ opacity: 0, x: -30 }}
+                    viewport={{ once: true }}
+                    whileInView={{ opacity: 1, x: 0 }}
+                    transition={{ delay: 0.8 }}
+                    src={`${baseUrl}/assets/3.9.png`} alt="Who memperkirakan" className="absolute bottom-0" />
+            </div>
+
+        </div>
+    </div>
+}
