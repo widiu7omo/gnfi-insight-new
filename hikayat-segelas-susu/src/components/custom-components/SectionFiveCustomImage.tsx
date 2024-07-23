86b170db0380550caebc75feb7fc03bdf63bde92
Naufal Rafianto <mnaufalrafianto@gmail.com>
2024-07-23 21:18:04 +0700

    add scetionfivecustomimage

--- hikayat-segelas-susu/src/components/custom-components/SectionFiveCustomImage.tsx
+++ hikayat-segelas-susu/src/components/custom-components/SectionFiveCustomImage.tsx
@@ -1,57 +1,110 @@
-"use client"
+"use client";
 
-import { baseUrl } from "@/constants/meta"
-import { motion } from "framer-motion"
+import { baseUrl } from "@/constants/meta";
+import { motion } from "framer-motion";
+import { useScroll, useTransform } from "framer-motion";
 
 export function SectionFiveCustomImage() {
-    return <div className="bg-cover relative xl:bg-[center_top_-30rem]" style={{ backgroundImage: `url(${baseUrl}/assets/5.1.png)` }}>
-        <div className="max-w-4xl mx-auto pt-0 xl:pt-6 z-20 relative ">
+  const { scrollY } = useScroll();
+  const y = useTransform(scrollY, [0, 500], [0, -100]);
+
+  return (
+    <section>
+      <div className="relative w-full">
+        {" "}
+        {/* Background Image */}
+        <motion.img
+          src={`${baseUrl}/assets/11.7.png`}
+          width={1079}
+          height={601}
+          alt="Background Cows"
+          className="w-full h-full object-cover object-center"
+          initial={{ scale: 1.1 }}
+          whileInView={{ scale: 1 }}
+          transition={{ duration: 1.5, ease: "easeOut" }}
+        />
+        {/* Gradient Overlay */}
+        <motion.div
+          className="absolute bg-gradient-to-b from-black/10 to-[#000] z-0 left-0 right-0 top-0 bottom-0 p-5"
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          transition={{ duration: 1.5, ease: "easeInOut" }}
+        />
+        {/* Main Title */}
+        <motion.div
+          className="bg-[#B58F47] z-0 absolute text-3xl top-28 left-14 text-white p-2 rounded-lg font-bold"
+          initial={{ opacity: 0, scale: 0.9 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 1, ease: "easeOut" }}
+        >
+          Susu (dan Makan Siang) Gratis
+        </motion.div>
+        {/* Description */}
+        <motion.div
+          className="z-0 absolute text-xl top-28 right-8 text-white p-2 rounded-lg w-96"
+          initial={{ opacity: 0, x: 50 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          transition={{ duration: 1, ease: "easeOut" }}
+        >
+          Calon Presiden dan Wakil Presiden terpilih, Prabowo Subianto dan
+          Gibran Rakabuming, punya program pemberian makanan dan susu gratis.
+        </motion.div>
+        {/* Main Heading */}
+        <motion.h2
+          initial={{ opacity: 0, y: 50 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          viewport={{ once: false }}
+          transition={{ delay: 0.3, duration: 1, ease: "easeOut" }}
+          className="w-full text-8xl font-bold text-white absolute top-48 left-10 -translate-x-1/2 -translate-y-1/2"
+        >
+          Akankah <br />
+          Terealisasi?
+        </motion.h2>
+        {/* Program Target */}
+        <motion.div
+          className="z-0 absolute text-xl top-72 bg-[#FFC949] right-14 text-black p-2 rounded-lg w-52"
+          initial={{ opacity: 0, scale: 0.8 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 1, ease: "easeOut" }}
+        >
+          Sasaran program:
+          <span className="font-bold text-4xl">
+            <br />
+            82.9 Juta
+            <br />
+          </span>
+          orang
+          <br />
+          yang terdiri dari anak-anak usia dini, murid SD, SMP, SMA/SMK, santri,
+          hingga ibu hamil.
+        </motion.div>
+      </div>
+
+      {/* Images Section */}
+      <div className="flex flex-col sm:flex-row items-center mx-auto max-w-6xl mt-5">
+        <div className="w-full">
+          <div className="relative flex items-end justify-center gap-x-10">
             <motion.img
-                initial={{ opacity: 0, y: 20 }}
-                viewport={{ once: true }}
-                whileInView={{ opacity: 1, y: 0 }}
-                transition={{ delay: 0.1 }}
-                src={`${baseUrl}/assets/5.2.png`} alt="Illustration" />
-
+              initial={{ opacity: 0, x: -50 }}
+              whileInView={{ opacity: 1, x: 0 }}
+              viewport={{ once: false }}
+              transition={{ delay: 1, duration: 1, ease: "easeOut" }}
+              src={`${baseUrl}/assets/11.5.png`}
+              alt="Eclipse"
+              className="w-2/5 sm:w-1/4 h-full"
+            />
+            <motion.img
+              initial={{ opacity: 0, x: 50 }}
+              whileInView={{ opacity: 1, x: 0 }}
+              viewport={{ once: false }}
+              transition={{ delay: 1.5, duration: 1, ease: "easeOut" }}
+              src={`${baseUrl}/assets/11.6.png`}
+              alt="Susu"
+              className="w-1/2 sm:w-1/4 h-full"
+            />
+          </div>
         </div>
-        <div className="" style={{ background: `url(${baseUrl}/assets/4.1.1.png),white` }}>
-            <div className="max-w-4xl mx-auto pt-0 xl:pt-6 z-20 relative]">
-                <div className="relative flex flex-col justify-end">
-                    <motion.img
-                        initial={{ opacity: 0, x: -30 }}
-                        viewport={{ once: true }}
-                        whileInView={{ opacity: 1, x: 0 }}
-                        transition={{ delay: 0.4 }}
-                        src={`${baseUrl}/assets/5.3.png`} alt="Title" className="-mt-[7rem] sm:-mt-[8rem] xl:-mt-[15rem] z-10" />
-                    <motion.img
-                        initial={{ opacity: 0, x: 30 }}
-                        viewport={{ once: true }}
-                        whileInView={{ opacity: 1, x: 0 }}
-                        transition={{ delay: 0.2 }}
-                        src={`${baseUrl}/assets/5.4.png`} alt="Thinking" className="absolute bottom-0" />
-
-                </div>
-                <div className="relative flex flex-col justify-end ">
-                    <motion.img
-                        initial={{ opacity: 0, y: 20 }}
-                        viewport={{ once: true }}
-                        whileInView={{ opacity: 1, y: 0 }}
-                        transition={{ delay: 0.5 }}
-                        src={`${baseUrl}/assets/5.5.png`} alt="Card 1" className="mix-blend-darken" />
-                    <motion.img
-                        initial={{ opacity: 0, y: 20 }}
-                        viewport={{ once: true }}
-                        whileInView={{ opacity: 1, y: 0 }}
-                        transition={{ delay: 0.5 }}
-                        src={`${baseUrl}/assets/5.6.png`} alt="Card 1" className="absolute" />
-                    <motion.img
-                        initial={{ opacity: 0, y: 20 }}
-                        viewport={{ once: true }}
-                        whileInView={{ opacity: 1, y: 0 }}
-                        transition={{ delay: 0.1 }}
-                        src={`${baseUrl}/assets/5.7.png`} alt="Card 2" className="absolute z-20 " />
-                </div>
-            </div>
-        </div>
-    </div>
+      </div>
+    </section>
+  );
 }
--- hikayat-segelas-susu/src/components/custom-components/SectionFourCustomImage.tsx
+++ hikayat-segelas-susu/src/components/custom-components/SectionFourCustomImage.tsx
@@ -1,12 +1,20 @@
 "use client";
 import { baseUrl } from "@/constants/meta";
-import { motion } from "framer-motion";
+import { motion, useScroll, useTransform } from "framer-motion";
 
 export function SectionFourCustomImage() {
+  const { scrollY } = useScroll();
+  const y = useTransform(scrollY, [0, 500], [0, -100]);
   return (
     <section>
-      <div className="relative w-full">
-        <img src={`${baseUrl}/assets/bg.png`} width={1079} height={601} alt="Background Cows" className="w-full h-full object-cover object-center" />
+      <motion.div style={{ y }} className="relative w-full">
+        <img
+          src={`${baseUrl}/assets/bg.png`}
+          width={1079}
+          height={601}
+          alt="Background Cows"
+          className="w-full h-full object-cover object-center"
+        />
         <div className="absolute bg-gradient-to-b from-black/10 to-[#FAFAFA] z-0 left-0 right-0 top-0 bottom-0" />
         <motion.h2
           initial={{ opacity: 0 }}
@@ -17,25 +25,113 @@ export function SectionFourCustomImage() {
         >
           Produksi vs Konsumsi <br /> Susu Indonesia
         </motion.h2>
-      </div>
+      </motion.div>
       <div className="flex flex-col sm:flex-row items-center mx-auto max-w-6xl">
         <div className="w-full">
           <div className="relative flex items-center justify-center">
-            <motion.img initial={{ opacity: 0, y: 20 }} viewport={{ once: false }} whileInView={{ opacity: 1, y: 0 }} transition={{ delay: 1.2 }} src={`${baseUrl}/assets/10.1.png`} alt="Eclipse" className="" />
-            <motion.img initial={{ opacity: 0, x: -20 }} viewport={{ once: false }} whileInView={{ opacity: 1, x: 0 }} transition={{ delay: 1.9 }} src={`${baseUrl}/assets/10.2.png`} alt="Susu" className="absolute z-10" />
-            <motion.img initial={{ opacity: 0, x: 20 }} viewport={{ once: false }} whileInView={{ opacity: 1, x: 0 }} transition={{ delay: 2.6 }} src={`${baseUrl}/assets/10.3.png`} alt="Info" className="absolute" />
-            <motion.img initial={{ opacity: 0, y: 20 }} viewport={{ once: false }} whileInView={{ opacity: 1, y: 0 }} transition={{ delay: 1.2 }} src={`${baseUrl}/assets/9.7.png`} alt="Eclipse" className="absolute" />
-            <motion.img initial={{ opacity: 0, y: -20 }} viewport={{ once: false }} whileInView={{ opacity: 1, y: 0 }} transition={{ delay: 1.9 }} src={`${baseUrl}/assets/9.4.png`} alt="Sapi" className="absolute" />
-            <motion.img initial={{ opacity: 0, x: -20 }} viewport={{ once: false }} whileInView={{ opacity: 1, x: 0 }} transition={{ delay: 2.6 }} src={`${baseUrl}/assets/9.5.png`} alt="Perah" className="absolute" />
-            <motion.img initial={{ opacity: 0, x: 20 }} viewport={{ once: false }} whileInView={{ opacity: 1, x: 0 }} transition={{ delay: 3.2 }} src={`${baseUrl}/assets/9.6.png`} alt="Info" className="absolute" />
+            <motion.img
+              initial={{ opacity: 0, y: 20 }}
+              viewport={{ once: false }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ delay: 1.2 }}
+              src={`${baseUrl}/assets/10.1.png`}
+              alt="Eclipse"
+              className=""
+            />
+            <motion.img
+              initial={{ opacity: 0, x: -20 }}
+              viewport={{ once: false }}
+              whileInView={{ opacity: 1, x: 0 }}
+              transition={{ delay: 1.9 }}
+              src={`${baseUrl}/assets/10.2.png`}
+              alt="Susu"
+              className="absolute z-10"
+            />
+            <motion.img
+              initial={{ opacity: 0, x: 20 }}
+              viewport={{ once: false }}
+              whileInView={{ opacity: 1, x: 0 }}
+              transition={{ delay: 2.6 }}
+              src={`${baseUrl}/assets/10.3.png`}
+              alt="Info"
+              className="absolute"
+            />
+            <motion.img
+              initial={{ opacity: 0, y: 20 }}
+              viewport={{ once: false }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ delay: 1.2 }}
+              src={`${baseUrl}/assets/9.7.png`}
+              alt="Eclipse"
+              className="absolute"
+            />
+            <motion.img
+              initial={{ opacity: 0, y: -20 }}
+              viewport={{ once: false }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ delay: 1.9 }}
+              src={`${baseUrl}/assets/9.4.png`}
+              alt="Sapi"
+              className="absolute"
+            />
+            <motion.img
+              initial={{ opacity: 0, x: -20 }}
+              viewport={{ once: false }}
+              whileInView={{ opacity: 1, x: 0 }}
+              transition={{ delay: 2.6 }}
+              src={`${baseUrl}/assets/9.5.png`}
+              alt="Perah"
+              className="absolute"
+            />
+            <motion.img
+              initial={{ opacity: 0, x: 20 }}
+              viewport={{ once: false }}
+              whileInView={{ opacity: 1, x: 0 }}
+              transition={{ delay: 3.2 }}
+              src={`${baseUrl}/assets/9.6.png`}
+              alt="Info"
+              className="absolute"
+            />
           </div>
         </div>
       </div>
       <div className="relative flex items-center justify-center">
-        <motion.img initial={{ opacity: 0, y: 20 }} viewport={{ once: false }} whileInView={{ opacity: 1, y: 0 }} transition={{ delay: 0.5 }} src={`${baseUrl}/assets/11.1.png`} alt="Background" className="" />
-        <motion.img initial={{ opacity: 0, y: -20 }} viewport={{ once: false }} whileInView={{ opacity: 1, y: 0 }} transition={{ delay: 1 }} src={`${baseUrl}/assets/11.2.png`} alt="Submer" className="absolute" />
-        <motion.img initial={{ opacity: 0, x: -20 }} viewport={{ once: false }} whileInView={{ opacity: 1, x: 0 }} transition={{ delay: 1.5 }} src={`${baseUrl}/assets/11.3.png`} alt="Chart" className="absolute" />
-        <motion.img initial={{ opacity: 0, x: 20 }} viewport={{ once: false }} whileInView={{ opacity: 1, x: 0 }} transition={{ delay: 2 }} src={`${baseUrl}/assets/11.4.png`} alt="Wilayah" className="absolute" />
+        <motion.img
+          initial={{ opacity: 0, y: 20 }}
+          viewport={{ once: false }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ delay: 0.5 }}
+          src={`${baseUrl}/assets/11.1.png`}
+          alt="Background"
+          className=""
+        />
+        <motion.img
+          initial={{ opacity: 0, y: -20 }}
+          viewport={{ once: false }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ delay: 1 }}
+          src={`${baseUrl}/assets/11.2.png`}
+          alt="Submer"
+          className="absolute"
+        />
+        <motion.img
+          initial={{ opacity: 0, x: -20 }}
+          viewport={{ once: false }}
+          whileInView={{ opacity: 1, x: 0 }}
+          transition={{ delay: 1.5 }}
+          src={`${baseUrl}/assets/11.3.png`}
+          alt="Chart"
+          className="absolute"
+        />
+        <motion.img
+          initial={{ opacity: 0, x: 20 }}
+          viewport={{ once: false }}
+          whileInView={{ opacity: 1, x: 0 }}
+          transition={{ delay: 2 }}
+          src={`${baseUrl}/assets/11.4.png`}
+          alt="Wilayah"
+          className="absolute"
+        />
       </div>
     </section>
   );
