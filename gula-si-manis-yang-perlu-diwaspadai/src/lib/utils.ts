125d44c291df71870e1de8cc10b25aaa0e01a939
Danar Widi <widiu7omo@gmail.com>
2024-08-20 14:42:13 +0800

    add carousel component

--- gula-si-manis-yang-perlu-diwaspadai/src/lib/utils.ts
+++ gula-si-manis-yang-perlu-diwaspadai/src/lib/utils.ts
@@ -1,4 +1,5 @@
 import { type ClassValue, clsx } from 'clsx'
+import { nanoid } from 'nanoid';
 import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
@@ -14,3 +15,7 @@ export const groupByToMap = <T, Q>(
     map.get(key)?.push(value) ?? map.set(key, [value]);
     return map;
   }, new Map<Q, T[]>());
+
+export function uniqueKey(): string {
+  return nanoid();
+}
