c4048ae527a69c280262c69aafecb4013d4888d2
anangyoga <aharwinto@gmail.com>
2024-08-21 14:48:53 +0700

    fix: add -mt

--- gula-si-manis-yang-perlu-diwaspadai/src/components/custom-components/SectionFourCustomImage.tsx
+++ gula-si-manis-yang-perlu-diwaspadai/src/components/custom-components/SectionFourCustomImage.tsx
@@ -18,7 +18,7 @@ export function SectionFourCustomImage() {
           Rekomendasi asupan gula harian:
         </motion.p>
       </div>
-      <div className="relative flex items-center justify-center w-full mx-auto">
+      <div className="relative lg:-mt-20 flex items-center justify-center w-full mx-auto">
         <motion.img initial={{ opacity: 0, y: 20 }} viewport={{ once: false }} whileInView={{ opacity: 1, y: 0 }} transition={{ delay: 0.8 }} src={`${baseUrl}/assets/kids.png`} alt="kids" className="absolute top-0 z-10" />
         <motion.img initial={{ opacity: 0, y: -20 }} viewport={{ once: false }} whileInView={{ opacity: 1, y: 0 }} transition={{ delay: 0.5 }} src={`${baseUrl}/assets/background.png`} alt="background" className="absolute top-0" />
         <motion.img initial={{ opacity: 0, x: 20 }} viewport={{ once: false }} whileInView={{ opacity: 1, x: 0 }} transition={{ delay: 1.3 }} src={`${baseUrl}/assets/anak.png`} alt="anak" className="absolute top-0" />
