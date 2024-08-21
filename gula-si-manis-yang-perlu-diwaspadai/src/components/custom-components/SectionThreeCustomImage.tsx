2dda343e109697e6a6d6854dd13a237da57154f3
Danar Widi <widiu7omo@gmail.com>
2024-08-21 13:57:01 +0800

    WIP: section 3

--- gula-si-manis-yang-perlu-diwaspadai/src/components/custom-components/SectionThreeCustomImage.tsx
+++ gula-si-manis-yang-perlu-diwaspadai/src/components/custom-components/SectionThreeCustomImage.tsx
@@ -4,12 +4,30 @@ import { motion } from "framer-motion";
 
 export function SectionThreeCustomImage() {
   return (
-    <div className="bg-cover bg-nore relative xl:bg-[center_top_-30rem]" style={{ backgroundImage: `url(${baseUrl}/assets/3.1.1.png)` }}>
-      <div className="absolute top-0 left-0 right-0 bottom-0 from-[5%] bg-gradient-to-b from-[#D5D5D5] to-[#606060]/0" >
+    <div className="flex flex-col">
+      <div className="flex items-center z-10 relative justify-center bg-cover bg-[center_bottom] bg-no-repeat" style={{ backgroundImage: `linear-gradient(rgba(0,0,0,.3) 0%,rgba(0,0,0,1) 120% ),url('${baseUrl}/assets/3.0.jpg')` }}>
+        <div className="justify-center max-w-7xl mx-auto ">
+          <img src={`${baseUrl}/assets/3.1.png`} alt="" className=" top-0" />
+          <img src={`${baseUrl}/assets/3.2.png`} alt="" className="absolute top-0" />
+        </div>
+      </div>
+      <div className="bg-white relative flex items-center justify-center">
+        <img src={`${baseUrl}/assets/3.0.1.png`} alt="" className="object-center" />
+        <img src={`${baseUrl}/assets/3.3.png`} alt="" className="absolute bottom-0" />
+        <img src={`${baseUrl}/assets/3.4.png`} alt="" className="absolute bottom-0" />
+        <img src={`${baseUrl}/assets/3.5.png`} alt="" className="absolute bottom-0" />
+        <img src={`${baseUrl}/assets/3.6.png`} alt="" className="absolute bottom-0" />
+        <img src={`${baseUrl}/assets/3.7.png`} alt="" className="absolute bottom-0" />
+        <img src={`${baseUrl}/assets/3.8.png`} alt="" className="absolute bottom-0" />
+        <img src={`${baseUrl}/assets/3.9.png`} alt="" className="absolute bottom-0" />
+        <img src={`${baseUrl}/assets/3.10.png`} alt="" className="absolute bottom-0" />
+        <img src={`${baseUrl}/assets/3.11.png`} alt="" className="absolute bottom-0" />
+        <div className="bg-gradient-to-b from-white/0 to-white absolute top-[90%] bottom-0 left-0 right-0" />
+      </div>
+      {/* <div className="absolute top-0 left-0 right-0 bottom-0 from-[5%] bg-gradient-to-b from-[#000000] to-[#000000]/0" >
       </div>
       <div className="max-w-4xl mx-auto">
         <div className="relative flex items-center justify-center">
-          {/* <div className="absolute bg-gradient-to-t from-[#04070B] z-0 left-0 right-0 top-[20%] bottom-0" /> */}
           <img src={`${baseUrl}/assets/3.1.png`} alt="Title" />
           <motion.img initial={{ opacity: 0, y: 20 }} viewport={{ once: true }} whileInView={{ opacity: 1, y: 0 }} transition={{ delay: 0.1 }} src={`${baseUrl}/assets/3.4.png`} className="absolute" alt="Title" />
           <motion.img initial={{ opacity: 0, y: 20 }} viewport={{ once: true }} whileInView={{ opacity: 1, y: 0 }} transition={{ delay: 0.1 }} src={`${baseUrl}/assets/3.3.png`} className="absolute" alt="Title" />
@@ -30,7 +48,7 @@ export function SectionThreeCustomImage() {
           <motion.img className="" initial={{ opacity: 0, y: 30 }} viewport={{ once: true }} whileInView={{ opacity: 1, y: 0 }} transition={{ delay: 0.5 }} src={`${baseUrl}/assets/3.9.png`} alt="Who memperkirakan" />
           <motion.img initial={{ opacity: 0, x: -30 }} viewport={{ once: false }} whileInView={{ opacity: 1, x: 0 }} transition={{ delay: 0.4 }} src={`${baseUrl}/assets/3.8.png`} alt="Who memperkirakan" className="absolute" />
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
