855ad290232d31efa8793807c14df2fa3b04cf8e
anangyoga <aharwinto@gmail.com>
2024-08-27 14:17:14 +0700

    feat: students image

--- ki-hajar-dewantara-sang-bapak-pendidikan-dan-sumbangan-pemikirannya-untuk-indonesia/src/components/custom-components/SectionFourCustomImage.tsx
+++ ki-hajar-dewantara-sang-bapak-pendidikan-dan-sumbangan-pemikirannya-untuk-indonesia/src/components/custom-components/SectionFourCustomImage.tsx
@@ -10,6 +10,11 @@ export function SectionFourCustomImage() {
         <motion.img initial={{ opacity: 0, x: -10 }} viewport={{ once: false }} whileInView={{ opacity: 1, x: 0 }} transition={{ delay: 1.3 }} src={`${baseUrl}/assets/quote.png`} alt="quote" className="absolute top-0" />
         <motion.img initial={{ opacity: 0, x: 10 }} viewport={{ once: false }} whileInView={{ opacity: 1, x: 0 }} transition={{ delay: 1.6 }} src={`${baseUrl}/assets/quote-description.png`} alt="quote" className="absolute top-0" />
         <motion.img initial={{ opacity: 0, y: 20 }} viewport={{ once: false }} whileInView={{ opacity: 1, y: 0 }} transition={{ delay: 1.9 }} src={`${baseUrl}/assets/quote-ki-hajar.png`} alt="quote" className="absolute top-0" />
+        <motion.img initial={{ opacity: 0, x: -20 }} viewport={{ once: false }} whileInView={{ opacity: 1, x1: 0 }} transition={{ delay: 2.2 }} src={`${baseUrl}/assets/student.png`} alt="quote" className="absolute top-0" />
+        <motion.img initial={{ opacity: 0, x: 20 }} viewport={{ once: false }} whileInView={{ opacity: 1, x: 0 }} transition={{ delay: 2.4 }} src={`${baseUrl}/assets/student-toga.png`} alt="quote" className="absolute top-0" />
+        <motion.img initial={{ opacity: 0, x: 20 }} viewport={{ once: false }} whileInView={{ opacity: 1, x: 0 }} transition={{ delay: 2.6 }} src={`${baseUrl}/assets/student-jacket.png`} alt="quote" className="absolute top-0" />
+        <motion.img initial={{ opacity: 0, x: -20 }} viewport={{ once: false }} whileInView={{ opacity: 1, x: 0 }} transition={{ delay: 2.8 }} src={`${baseUrl}/assets/student-jilbab.png`} alt="quote" className="absolute top-0" />
+        <motion.img initial={{ opacity: 0, y: -20 }} viewport={{ once: false }} whileInView={{ opacity: 1, y: 0 }} transition={{ delay: 2.1 }} src={`${baseUrl}/assets/students.png`} alt="quote" className="absolute top-0" />
         <motion.img initial={{ opacity: 0, y: 20 }} viewport={{ once: false }} whileInView={{ opacity: 1, y: 0 }} transition={{ delay: 0.8 }} src={`${baseUrl}/assets/elipse-bawah.png`} alt="elipse-bawah" className="w-full h-full" />
       </div>
     </section>
