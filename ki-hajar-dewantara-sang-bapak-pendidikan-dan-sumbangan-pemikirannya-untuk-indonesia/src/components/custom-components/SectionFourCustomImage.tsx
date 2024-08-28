4114a0cf1dcd7d00fbc872143119f707cc9fb84d
Danar Widi <widiu7omo@gmail.com>
2024-08-28 17:07:20 +0800

    WIP: section 3

--- ki-hajar-dewantara-sang-bapak-pendidikan-dan-sumbangan-pemikirannya-untuk-indonesia/src/components/custom-components/SectionFourCustomImage.tsx
+++ ki-hajar-dewantara-sang-bapak-pendidikan-dan-sumbangan-pemikirannya-untuk-indonesia/src/components/custom-components/SectionFourCustomImage.tsx
@@ -1,40 +1,47 @@
 "use client";
 import { baseUrl } from "@/constants/meta";
-import { motion, useScroll, useTransform } from "framer-motion";
+import { motion } from "framer-motion";
 
 export function SectionFourCustomImage() {
   return (
-    <section className="h-[1350px] sm:h-[2200px] relative">
-      <div className="relative w-full h-[500px] bg-cover bg-center flex items-center justify-center" style={{ backgroundImage: `url(${baseUrl}/assets/header-image.png)` }}>
-        <motion.p initial={{ opacity: 0, y: 20 }} viewport={{ once: true }} whileInView={{ opacity: 1, y: 0 }} transition={{ delay: 0.3 }} className="text-3xl font-bold text-center lg:text-4xl xl:text-5xl">
-          Hati-hati, <br /> Jangan Berlebihan <br /> Mengonsumsi Gula!
-        </motion.p>
-      </div>
-      <div className="mt-14 flex flex-col gap-4 sm:gap-10">
-        <motion.p initial={{ opacity: 0, y: 20 }} viewport={{ once: true }} whileInView={{ opacity: 1, y: 0 }} transition={{ delay: 0.9 }} className="text-center text-lg sm:text-xl font-semibold px-5 sm:max-w-xl sm:mx-auto">
-          Konsumsi gula berlebih dapat menimbulkan penyakit, salah satunya diabetes. Oleh karena itu, asupannya harus dibatasi
-        </motion.p>
-        <motion.p initial={{ opacity: 0, y: 20 }} viewport={{ once: true }} whileInView={{ opacity: 1, y: 0 }} transition={{ delay: 1 }} className="text-center text-base sm:text-lg font-medium">
-          Rekomendasi asupan gula harian:
-        </motion.p>
-      </div>
-      <div className="relative lg:-mt-20 flex items-center justify-center w-full mx-auto">
-        <motion.img initial={{ opacity: 0, y: 20 }} viewport={{ once: false }} whileInView={{ opacity: 1, y: 0 }} transition={{ delay: 0.8 }} src={`${baseUrl}/assets/kids.png`} alt="kids" className="absolute top-0 z-10" />
-        <motion.img initial={{ opacity: 0, y: -20 }} viewport={{ once: false }} whileInView={{ opacity: 1, y: 0 }} transition={{ delay: 0.5 }} src={`${baseUrl}/assets/background.png`} alt="background" className="absolute top-0" />
-        <motion.img initial={{ opacity: 0, x: 20 }} viewport={{ once: false }} whileInView={{ opacity: 1, x: 0 }} transition={{ delay: 1.3 }} src={`${baseUrl}/assets/anak.png`} alt="anak" className="absolute top-0" />
-        <motion.img initial={{ opacity: 0, x: -20 }} viewport={{ once: false }} whileInView={{ opacity: 1, x: 0 }} transition={{ delay: 1.2 }} src={`${baseUrl}/assets/dewasa.png`} alt="dewasa" className="absolute top-0" />
-        <motion.img initial={{ opacity: 0, x: 20 }} viewport={{ once: false }} whileInView={{ opacity: 1, x: 0 }} transition={{ delay: 1.4 }} src={`${baseUrl}/assets/layer11.png`} alt="layer11" className="absolute top-0 z-20" />
-        <motion.img initial={{ opacity: 0, x: 20 }} viewport={{ once: false }} whileInView={{ opacity: 1, x: 0 }} transition={{ delay: 1.8 }} src={`${baseUrl}/assets/layer13.png`} alt="layer13" className="absolute top-0 z-20" />
-        <motion.img initial={{ opacity: 0, x: 20 }} viewport={{ once: false }} whileInView={{ opacity: 1, x: 0 }} transition={{ delay: 2 }} src={`${baseUrl}/assets/layer15.png`} alt="layer15" className="absolute top-0 z-20" />
-        <motion.img initial={{ opacity: 0, y: 20 }} viewport={{ once: false }} whileInView={{ opacity: 1, x: 0 }} transition={{ delay: 1 }} src={`${baseUrl}/assets/description.png`} alt="description" className="absolute -top-6 z-10" />
-        <motion.img initial={{ opacity: 0, x: -20 }} viewport={{ once: false }} whileInView={{ opacity: 1, x: 0 }} transition={{ delay: 2.4 }} src={`${baseUrl}/assets/blood.png`} alt="blood" className="absolute top-0 z-20" />
-      </div>
-      <div className="flex items-center justify-center w-full mx-auto">
-        <motion.img initial={{ opacity: 0, x: -30 }} viewport={{ once: false }} whileInView={{ opacity: 1, x: 0 }} transition={{ delay: 1.8 }} src={`${baseUrl}/assets/doctor_bg.png`} alt="doctor_bg" className="absolute bottom-0" />
-        <motion.img initial={{ opacity: 0, y: 30 }} viewport={{ once: false }} whileInView={{ opacity: 1, y: 0 }} transition={{ delay: 2.5 }} src={`${baseUrl}/assets/doctor_quote.png`} alt="doctor_quote" className="absolute bottom-0" />
-        <motion.img initial={{ opacity: 0, x: -30 }} viewport={{ once: false }} whileInView={{ opacity: 1, x: 0 }} transition={{ delay: 2 }} src={`${baseUrl}/assets/quote_top.png`} alt="quote_top" className="absolute bottom-0" />
-        <motion.img initial={{ opacity: 0, x: 30 }} viewport={{ once: false }} whileInView={{ opacity: 1, x: 0 }} transition={{ delay: 2.2 }} src={`${baseUrl}/assets/quote_bottom.png`} alt="quote_bottom" className="absolute bottom-0" />
-        <motion.img initial={{ opacity: 0, y: 30 }} viewport={{ once: false }} whileInView={{ opacity: 1, y: 0 }} transition={{ delay: 2.9 }} src={`${baseUrl}/assets/doctor.png`} alt="doctor" className="absolute bottom-0" />
+    <section className="relative">
+      <div className="flex flex-col bg-[hsl(30,40%,90%)]">
+        <div className="flex flex-col z-10 relative bg-cover bg-[center_top] bg-no-repeat" style={{ backgroundImage: `url('${baseUrl}/assets/3.0.png')` }}>
+          <div className="flex justify-center max-w-4xl mx-auto relative lg:mt-[27rem]">
+            <motion.img
+              initial={{ opacity: 0, y: 20 }} viewport={{ once: true }}
+              whileInView={{ opacity: 1, y: 0 }} transition={{ duration: .7, delay: 0.1 }}
+              src={`${baseUrl}/assets/3.1.png`} alt="" className="top-0" />
+          </div>
+          <div className="flex justify-center max-w-4xl mx-auto relative xl:mt-[5rem]">
+            <motion.img
+              initial={{ opacity: 0, y: 20 }} viewport={{ once: true }}
+              whileInView={{ opacity: 1, y: 0 }} transition={{ duration: .7, delay: 0.1 }}
+              src={`${baseUrl}/assets/3.2.png`} alt="" className="top-0" />
+            <motion.img
+              initial={{ opacity: 0, y: 20 }} viewport={{ once: true }}
+              whileInView={{ opacity: 1, y: 0 }} transition={{ duration: .7, delay: 0.1 }}
+              src={`${baseUrl}/assets/3.3.png`} alt="" className="absolute top-0" />
+            <motion.img
+              initial={{ opacity: 0, y: 20 }} viewport={{ once: true }}
+              whileInView={{ opacity: 1, y: 0 }} transition={{ duration: .7, delay: 0.1 }}
+              src={`${baseUrl}/assets/3.4.png`} alt="" className="absolute top-0 lg:top-[8rem]" />
+            <motion.img
+              initial={{ opacity: 0, y: 20 }} viewport={{ once: true }}
+              whileInView={{ opacity: 1, y: 0 }} transition={{ duration: .7, delay: 0.1 }}
+              src={`${baseUrl}/assets/3.5.png`} alt="" className="absolute top-0 lg:top-[8rem]" />
+          </div>
+          <div className="flex justify-center max-w-4xl mx-auto relative lg:top-[8rem]">
+            <motion.img
+              initial={{ opacity: 0, y: 20 }} viewport={{ once: true }}
+              whileInView={{ opacity: 1, y: 0 }} transition={{ duration: .7, delay: 0.1 }}
+              src={`${baseUrl}/assets/3.6.png`} alt="" className="top-0" />
+            <motion.img
+              initial={{ opacity: 0, y: 20 }} viewport={{ once: true }}
+              whileInView={{ opacity: 1, y: 0 }} transition={{ duration: .7, delay: 0.1 }}
+              src={`${baseUrl}/assets/3.7.png`} alt="" className="absolute top-0" />
+          </div>
+        </div>
       </div>
     </section>
   );
