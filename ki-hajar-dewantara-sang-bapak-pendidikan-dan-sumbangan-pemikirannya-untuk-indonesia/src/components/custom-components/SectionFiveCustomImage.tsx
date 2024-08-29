c715838e4b68bc496fd9c72eb1bef624e27dfbef
Danar Widi <widiu7omo@gmail.com>
2024-08-29 17:09:44 +0800

    insight: ki hajar: section 5

--- ki-hajar-dewantara-sang-bapak-pendidikan-dan-sumbangan-pemikirannya-untuk-indonesia/src/components/custom-components/SectionFiveCustomImage.tsx
+++ ki-hajar-dewantara-sang-bapak-pendidikan-dan-sumbangan-pemikirannya-untuk-indonesia/src/components/custom-components/SectionFiveCustomImage.tsx
@@ -4,90 +4,31 @@ import { baseUrl } from "@/constants/meta";
 import { motion } from "framer-motion";
 
 export function SectionFiveCustomImage() {
-  const containerVariants = {
-    hidden: { opacity: 0 },
-    visible: {
-      opacity: 1,
-      transition: {
-        staggerChildren: 0.2,
-        delayChildren: 0.3,
-      },
-    },
-  };
-
-  const itemVariants = {
-    hidden: { opacity: 0, y: 20, scale: 0.8 },
-    visible: {
-      opacity: 1,
-      y: 0,
-      scale: 1,
-      transition: {
-        type: "spring",
-        stiffness: 100,
-        damping: 10,
-      },
-    },
-  };
-
   return (
-    <motion.section
-      className="relative max-w-4xl h-screen sm:h-[175vh] mx-auto flex justify-center items-center"
-      variants={containerVariants}
-      initial="hidden"
-      whileInView="visible"
-      viewport={{ once: false, amount: 0.3 }}
-    >
-      <div className="relative w-full h-full grid place-items-center">
-        {/* Title 1 */}
-        <motion.img
-          variants={itemVariants}
-          src={`${baseUrl}/assets/title-1.png`}
-          alt="Can"
-          className="absolute z-20 top-0 left-0 w-1/2 sm:w-2/5 md:w-1/3"
-        />
-        {/* Title 2 */}
-        <motion.img
-          variants={itemVariants}
-          src={`${baseUrl}/assets/title-2.png`}
-          alt="Can"
-          className="absolute z-20 top-1/4 right-0 w-2/5 sm:w-1/3 md:w-1/4"
-        />
-        {/* Layer 1 */}
-        <motion.img
-          variants={itemVariants}
-          src={`${baseUrl}/assets/Can.png`}
-          alt="Can"
-          className="absolute z-10 top-0 right-0 inset-0 mx-auto w-3/4 sm:w-2/3 md:w-1/2"
-        />
-        {/* Layer 2 */}
-        <motion.img
-          variants={itemVariants}
-          src={`${baseUrl}/assets/Layer 12.png`}
-          alt="Layer 12"
-          className="absolute z-20 w-1/5 sm:w-1/4 md:w-1/5 bottom-[15%] "
-        />
-        {/* Layer 3 */}
-        <motion.img
-          variants={itemVariants}
-          src={`${baseUrl}/assets/Group 1.png`}
-          alt="Group 1"
-          className="absolute z-30 sm:right-0 w-2/5 md:w-1/3 bottom-[15%] right-5"
-        />
-        {/* Layer 4 */}
-        <motion.img
-          variants={itemVariants}
-          src={`${baseUrl}/assets/Group 2.png`}
-          alt="Group 2"
-          className="absolute z-40 sm:left-0 w-2/5 md:w-1/3 bottom-[15%] left-5"
-        />
-        {/* Layer 5 */}
-        <motion.img
-          variants={itemVariants}
-          src={`${baseUrl}/assets/Group 3.png`}
-          alt="Group 3"
-          className="absolute z-50 bottom-0 w-full sm:w-3/4 md:w-2/3"
-        />
+    <section className="relative flex flex-col items-center bg-cover bg-top bg-no-repeat" style={{ backgroundImage: `url(${baseUrl}/assets/4.0.png)` }}>
+      <div className="max-w-4xl mx-auto relative">
+        <div className="flex items-center justify-center">
+          <img src={`${baseUrl}/assets/4.1.png`} alt="elipse-atas" className="top-0 z-10" />
+          <img src={`${baseUrl}/assets/4.2.png`} alt="elipse-atas" className="absolute top-0 z-10" />
+          <img src={`${baseUrl}/assets/4.3.png`} alt="elipse-atas" className="absolute top-0 z-10" />
+          <img src={`${baseUrl}/assets/4.4.png`} alt="elipse-atas" className="absolute top-0 z-10" />
+          <img src={`${baseUrl}/assets/4.5.png`} alt="elipse-atas" className="absolute top-0 z-10" />
+        </div>
+        <div className="flex items-center justify-center">
+          <img src={`${baseUrl}/assets/4.10.png`} alt="elipse-atas" className="absolute bottom-0 z-10" />
+          <img src={`${baseUrl}/assets/4.9.png`} alt="elipse-atas" className="absolute bottom-0 z-10" />
+          <img src={`${baseUrl}/assets/4.8.png`} alt="elipse-atas" className="absolute bottom-0 z-10" />
+          <img src={`${baseUrl}/assets/4.7.png`} alt="elipse-atas" className="absolute bottom-0 z-10" />
+          <img src={`${baseUrl}/assets/4.6.png`} alt="elipse-atas" className="top-0 z-10" />
+        </div>
       </div>
-    </motion.section>
+      <div className="flex items-center justify-center absolute bottom-0 w-full">
+        <img src={`${baseUrl}/assets/4.11.png`} alt="elipse-atas" className="absolute w-full bottom-0 left-0 right-0 z-10" />
+        <img src={`${baseUrl}/assets/4.12.png`} alt="elipse-atas" className="absolute w-full bottom-0 left-0 right-0 z-10" />
+        <img src={`${baseUrl}/assets/4.13.png`} alt="elipse-atas" className="absolute w-full bottom-0 left-0 right-0 z-10" />
+        <img src={`${baseUrl}/assets/4.14.png`} alt="elipse-atas" className="absolute w-full bottom-0 left-0 right-0 z-10" />
+        <img src={`${baseUrl}/assets/4.15.png`} alt="elipse-atas" className="absolute w-full bottom-0 left-0 right-0 z-10" />
+      </div>
+    </section>
   );
 }
