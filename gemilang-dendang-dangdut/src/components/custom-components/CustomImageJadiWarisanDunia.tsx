92892aca18a3e4056d46a4ad1ca024c67dbd09e2
anangyoga <aharwinto@gmail.com>
2024-10-09 14:25:50 +0700

    wip: Cafe Dangdut section

--- gemilang-dendang-dangdut/src/components/custom-components/CustomImageJadiWarisanDunia.tsx
+++ gemilang-dendang-dangdut/src/components/custom-components/CustomImageJadiWarisanDunia.tsx
@@ -5,7 +5,7 @@ import { motion } from "framer-motion";
 
 export function CustomImageJadiWarisanDunia() {
   return (
-    <section id="section-1" className="">
+    <section id="section-1" className="relative">
       <div className="relative">
         <div className="relative">
           <img src={`${baseUrl}/assets/7.0.png`} alt="people festival" className="w-full h-full relative inset-0 object-cover" />
@@ -31,6 +31,26 @@ export function CustomImageJadiWarisanDunia() {
             </motion.p>
             <motion.p initial={{ opacity: 0, y: 0, x: 5 }} whileInView={{ opacity: 1, y: 0, x: 0 }} viewport={{ once: true }} transition={{ delay: 0.8, duration: 1.3 }}>
               Tidak hanya di Indonesia, <br /> hal ini dilakukan <br /> beberapa orang <br /> ke pasar luar negeri.
+            </motion.p>
+          </div>
+        </div>
+      </div>
+      <div className="relative">
+        <img src={`${baseUrl}/assets/7.1.png`} alt="background image cafe dangdut" className="w-full" />
+        <motion.img
+          initial={{ opacity: 0, y: 5 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          viewport={{ once: true }}
+          transition={{ delay: 0.5 }}
+          src={`${baseUrl}/assets/7.2.png`}
+          alt="pedangdut Fitri Carlina"
+          className="absolute bottom-0 z-10 w-full"
+        />
+        <div className="absolute bottom-0 w-full">
+          <div className="relative flex items-center justify-end sm:justify-center">
+            <motion.img initial={{ opacity: 0, x: 5 }} whileInView={{ opacity: 1, x: 0 }} viewport={{ once: true }} transition={{ delay: 0.1 }} src={`${baseUrl}/assets/7.3.png`} alt="background hitam" className="relative w-full" />
+            <motion.p initial={{ opacity: 0, x: 5 }} whileInView={{ opacity: 1, x: 0 }} viewport={{ once: true }} transition={{ delay: 0.6 }} className="text-white absolute text-sm max-w-xl lg:text-lg font-semibold lg:tracking-widest z-10">
+              Hal ini dilihat dari bisnis Cafe Dangdut yang dibuka oleh penyanyi dangdut Fitri Carlina. Cafe yang menyajikan kopi asal Indonesia ini dibuka olehnya di New York Amerika Serikat.
             </motion.p>
           </div>
         </div>
