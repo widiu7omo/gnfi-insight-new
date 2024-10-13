6262ea28a7d9b17c66c0c8e31aee44aff581bcc8
Danar Widi <widiu7omo@gmail.com>
2024-10-13 09:26:14 +0800

    fix:responsive

--- gemilang-dendang-dangdut/src/components/custom-components/CustomImageAsalUsul.tsx
+++ gemilang-dendang-dangdut/src/components/custom-components/CustomImageAsalUsul.tsx
@@ -26,15 +26,16 @@ export function CustomImageAsalUsul() {
   }
 
   return (
-    <section id="section-1" className="bg-[#fffcf3] -mt-1 overflow-clip ">
-      <div className="relative flex items-start justify-center overflow-clip">
+    <section id="section-1" className="bg-[#fffcf3] -mt-1 overflow-clip">
+      <div className="relative flex items-start justify-center">
         <div
           className="w-full bg-no-repeat bg-[cover,contain] xl:bg-[cover,auto] bg-[center_top] relative bg-blend-multiply"
           style={{ backgroundImage: `url('${baseUrl}/assets/1.0.png')` }}
         >
-          <div className="absolute top-0 h-[10%] bg-gradient-to-b from-[#cec5be]  left-0 right-0" />
-          <img src={`${baseUrl}/assets/1.1.png`} alt="Bg 1" className="absolute top-0 lg:top-[-200px] xl:top-[-400px] 2xl:top-[-580px] left-0 right-0 w-full" />
-          <div className="max-w-4xl mx-auto relative z-10 top-0 mb-[20rem] sm:mb-0">
+          <div className="absolute top-0 h-[10%] bg-gradient-to-b from-[#cec5be] left-0 right-0" />
+          <div className="absolute top-0 bottom-0 w-full bg-gradient-to-b to-[#cec5be] from-[#cec5be]/40 md:hidden" />
+          <img src={`${baseUrl}/assets/1.1.png`} alt="Bg 1" className="absolute hidden xl:block top-0 lg:top-[-200px] xl:top-[-400px] 2xl:top-[-580px] left-0 right-0 w-full" />
+          <div className="max-w-4xl mx-auto relative z-10 top-0">
             <div className="space-y-0">
               <div className="relative flex flex-col items-center justify-center z-0">
                 <div className="mt-[10%] relative z-10">
@@ -45,7 +46,7 @@ export function CustomImageAsalUsul() {
                     transition={{ delay: 0.4, duration: 0.3 }}
                     src={`${baseUrl}/assets/1.2.png`}
                     className="relative"
-                    alt="Silas Papare"
+                    alt="Dang Dut"
                   />
                   <motion.img
                     initial={{ opacity: 0, y: 5 }}
@@ -57,8 +58,18 @@ export function CustomImageAsalUsul() {
                     alt="Title"
                   />
                 </div>
-                <div className=" relative z-0">
-                  <div className="relative opacity-0 sm:opacity-100">
+                <div className="md:hidden space-y-[10rem] px-4 pt-[12rem] sm:pt-[10rem]">
+                  <div className="relative">
+                    <motion.img variants={childVariants} className="absolute h-[6rem] left-0 top-[-6rem]" src={`${baseUrl}/assets/1.3.1.png`} />
+                    <motion.p variants={childVariants} className="text-2xl font-semibold">Penamaan dangdut merupakan pembentukan kata yang menirukan suara gendang, yaitu “dang”</motion.p>
+                  </div>
+                  <div className="relative">
+                    <motion.img variants={childVariants} className="absolute h-[5rem] left-0 top-[-5rem]" src={`${baseUrl}/assets/1.3.2.png`} />
+                    <motion.p variants={childVariants} className="text-2xl font-semibold">“dut” menjadi ungkapan yang digunakan untuk menghina dari lapisan masyarakat kelas atas kepada rakyat miskin</motion.p>
+                  </div>
+                </div>
+                <div className="relative z-0">
+                  <div className="relative opacity-0 md:opacity-100 h-[4rem] md:h-auto">
                     <motion.img
                       initial={{
                         x: 40,
@@ -81,27 +92,27 @@ export function CustomImageAsalUsul() {
                       alt="Rhoma Irama"
                     />
                   </div>
-                  <div className="flex flex-col sm:flex-row absolute top-0 sm:top-[20rem]">
+                  <div className="flex w-fit flex-col sm:flex-row absolute top-0 sm:top-[20rem]">
                     <motion.div
                       initial="pClose"
                       animate="pOpen"
                       variants={variants}
                       viewport={{ once: true }}
                       transition={{ delay: 0.4, duration: 0.4 }}
-                      className="flex-1 pl-[2rem] pt-[2rem] sm:pt-0 max-w-[50%] space-y-[30%]">
+                      className="flex-1 pl-[2rem] pt-[2rem] sm:pt-0 max-w-[50%] space-y-[30%] md:block hidden">
                       <div className="relative">
-                        <motion.img variants={childVariants} className="text-lg absolute left-[-5%] top-[-120%]" src={`${baseUrl}/assets/1.3.1.png`} />
+                        <motion.img variants={childVariants} className="size-auto absolute left-[-5%] top-[-120%]" src={`${baseUrl}/assets/1.3.1.png`} />
                         <motion.p variants={childVariants} className="text-2xl font-semibold">Penamaan dangdut merupakan pembentukan kata yang menirukan suara gendang, yaitu “dang”</motion.p>
                       </div>
                       <div className="relative">
-                        <motion.img variants={childVariants} className="text-lg absolute left-[-5%] top-[-80%]" src={`${baseUrl}/assets/1.3.2.png`} />
+                        <motion.img variants={childVariants} className="size-auto absolute left-[-5%] top-[-80%]" src={`${baseUrl}/assets/1.3.2.png`} />
                         <motion.p variants={childVariants} className="text-2xl font-semibold">“dut” menjadi ungkapan yang digunakan untuk menghina dari lapisan masyarakat kelas atas kepada rakyat miskin</motion.p>
                       </div>
                     </motion.div>
                   </div>
-                  <div className="flex">
+                  <div className="flex md:flex-row flex-col space-y-4 md:space-y-0">
                     <div className="flex-1">
-                      <p className="text-5xl font-semibold text-white px-8 tracking-normal leading-[3.7rem]">Konon, istilah dangdut
+                      <p className="text-3xl md:text-5xl font-semibold text-white px-4 md:px-8 tracking-normal md:leading-[3.7rem]">Konon, istilah dangdut
                         dipercaya muncul
                         pertama kali pada tahun <span className="text-[#ed8906]">1970-an</span></p>
                     </div>
