cc0ca3ec4ce68a42b944ad78dbce306283939390
Danar Widi <widiu7omo@gmail.com>
2024-11-15 08:15:48 +0800

    improve: section 5

--- injourney-airports-penghubung-dunia-kebanggaan-indonesia/src/components/custom-components/CustomImageSection5.tsx
+++ injourney-airports-penghubung-dunia-kebanggaan-indonesia/src/components/custom-components/CustomImageSection5.tsx
@@ -5,7 +5,7 @@ import { motion } from "framer-motion";
 
 export function CustomImageSection5() {
   return (
-    <div className="relative flex flex-col items-center overflow-hidden bg-cover">
+    <div className="relative flex flex-col items-center overflow-hidden bg-cover font-sora">
       <div className="relative w-full">
         <motion.img
           viewport={{ once: true }}
@@ -16,15 +16,15 @@ export function CustomImageSection5() {
           alt="Airport"
           className="z-0 object-cover w-full h-auto"
         />
-        <div className="absolute inset-0 z-10 bg-white bg-opacity-60" />
-        <div className="absolute top-0 z-10 flex justify-start w-full pt-12 transform md:bottom-auto">
+        <div className="absolute inset-0 z-10 bg-gradient-to-b h-[20%] from-[#f9f9fa]" />
+        <div className="absolute bottom-0 z-10 flex justify-start w-full transform pb-4">
           <div className="flex flex-col items-start w-full px-4 mx-auto space-y-4 md:max-w-4xl md:space-y-8 md:px-10">
             <motion.p
               viewport={{ once: true }}
               initial={{ opacity: 0, x: 10, y: 0 }}
               whileInView={{ opacity: 1, x: 0, y: 0 }}
               transition={{ delay: 0.3, duration: 0.4 }}
-              className="max-w-[80%] text-left text-xl font-extrabold text-[#131313] md:max-w-[50%] md:pt-6 md:text-6xl"
+              className="max-w-[80%] text-left text-xl font-sora font-extrabold text-[#131313] md:max-w-[50%] md:pt-6 md:text-6xl"
             >
               InJourney Airports di Kancah Internasional
             </motion.p>
@@ -33,7 +33,7 @@ export function CustomImageSection5() {
               initial={{ opacity: 0, x: -10, y: -10 }}
               whileInView={{ opacity: 1, x: 0, y: 0 }}
               transition={{ delay: 0.3, duration: 0.4 }}
-              className="mt-2 max-w-[90%] text-left text-base font-semibold text-[#131313] md:max-w-lg md:text-2xl"
+              className="mt-2 max-w-[90%] text-left text-base font-sora font-semibold text-[#131313] md:max-w-lg md:text-2xl"
             >
               Peningkatan pelayanan dan kerja sama seluas-luasnya, itulah dua
               senjata InJourney Airports untuk berkibar sampai ke mancanegara
@@ -44,20 +44,18 @@ export function CustomImageSection5() {
       <div className="relative flex w-full items-center justify-center bg-gradient-to-r from-[#0c9fad] to-[#066e74] pt-4 md:pt-20">
         <div className="relative flex justify-between w-full px-4 mx-auto md:max-w-4xl md:space-x-4 md:px-10">
           <div className="relative z-40 w-1/2 my-auto text-white space-y-4 md:mb-48 md:max-w-md">
-            <p className="text-base font-semibold leading-relaxed text-left md:text-xl">
-              <span className="text-2xl font-bold text-[#131313]">"</span>
+            <div className="text-base font-semibold leading-relaxed text-left md:text-lg">
+              <img src={`${baseUrl}/assets/quote.png`} alt="Quote Start" className="size-8 absolute top-[-2%] left-[-10%]" />
               Kami mengubah pendekatan dari yang berfokus pada operasional
               menjadi
               <span className="italic"> customer-oriented</span>, sehingga
               bandara bisa lebih proaktif dalam melayani pengguna. Perubahan
               mindset ini akan membuat kami lebih siap bersaing di pasar
               internasional.
-              <span className="text-2xl font-bold leading-none text-[#131313]">
-                "
-              </span>
-            </p>
-            <div className="mt-12 bg-[#0c9fad] px-4 py-4">
-              <p className="text-base font-semibold text-white md:text-base md:font-bold">
+              <img src={`${baseUrl}/assets/quote.png`} alt="Quote End" className="size-8 rotate-180 absolute bottom-[40%] right-[-10%]" />
+            </div>
+            <div className="mt-12 bg-[#0c9fad] px-4 py-4 -ml-4">
+              <p className="text-base font-semibold text-white md:text-xl md:font-bold">
                 Pgs. Corporate Secretary Group Head InJourney Airports, Arie
                 Ahsanurrohim
               </p>
@@ -74,45 +72,33 @@ export function CustomImageSection5() {
               className="absolute bottom-0 z-20 h-auto -right-[2rem] md:right-0 overflow-hidden object-cover md:max-w-7xl"
             />
           </div>
+
+        </div>
+        <div className="absolute bottom-[-45%] z-30 max-w-7xl mx-auto">
+          <motion.img
+            viewport={{ once: true }}
+            initial={{ opacity: 0, x: 0, y: 10 }}
+            whileInView={{ opacity: 1, x: 0, y: 0 }}
+            transition={{ delay: 0.3, duration: 0.4 }}
+            src={`${baseUrl}/assets/5.3.png`}
+            alt="Hand"
+            className=" h-auto w-full object-cover"
+          />
         </div>
       </div>
-      <div className="relative flex flex-col justify-center w-full">
-        <motion.img
-          viewport={{ once: true }}
-          initial={{ opacity: 0, x: 0, y: 10 }}
-          whileInView={{ opacity: 1, x: 0, y: 0 }}
-          transition={{ delay: 0.3, duration: 0.4 }}
-          src={`${baseUrl}/assets/5.4.png`}
-          alt="Airport2"
-          className="z-10 min-h-[36rem] w-auto object-cover lg:h-auto lg:w-full"
-        />
-        <motion.img
-          viewport={{ once: true }}
-          initial={{ opacity: 0, x: 0, y: 10 }}
-          whileInView={{ opacity: 1, x: 0, y: 0 }}
-          transition={{ delay: 0.3, duration: 0.4 }}
-          src={`${baseUrl}/assets/5.3.png`}
-          alt="Hand"
-          className="absolute -top-[8rem] z-30 h-auto w-full object-cover md:-top-[18rem]"
-        />
-        <div className="absolute inset-0 z-30 flex items-center justify-center px-4">
-          <div className="flex flex-col w-full space-y-12 md:max-w-4xl md:px-10 lg:space-y-8">
+      <div className="relative flex flex-col justify-center w-full bg-bottom" style={{ backgroundImage: `url(${baseUrl}/assets/5.4.png)` }}>
+        <div className="inset-0 z-30 flex items-center justify-center px-4 pb-[10rem]">
+          <div className="flex flex-col w-full space-y-12 md:max-w-4xl md:px-10 lg:space-y-[4rem]">
             <motion.p
               viewport={{ once: true }}
               initial={{ opacity: 0, x: 0, y: -10 }}
               whileInView={{ opacity: 1, x: 0, y: 0 }}
               transition={{ delay: 0.3, duration: 0.4 }}
-              className="w-full mx-auto text-base font-semibold text-center text-white md:px-15 md:mt-4 md:max-w-lg md:text-2xl md:font-bold"
+              className="w-full text-xl font-semibold text-center text-white md:mt-4 md:text-3xl md:font-bold "
             >
-              {"Sebagian inisiatif kerja sama internasional yang telah ditempuh InJourney Airports"
-                .split("")
-                .map((char, index) => (
-                  <span key={index} className="bg-[#ee6a57]">
-                    {char}
-                  </span>
-                ))}
+              <span className="bg-[#ee6a57] box-decoration-clone px-4">Sebagian inisiatif kerja sama internasional yang telah ditempuh InJourney Airports</span>
             </motion.p>
-            <div className="items-stretch min-h-full grid grid-cols-2 gap-4 md:gap-16">
+            <div className="items-stretch min-h-full grid grid-cols-2 gap-4 md:gap-16  pt-[10rem] ">
               <motion.div
                 viewport={{ once: true }}
                 initial={{ opacity: 0, x: 0, y: 10 }}
@@ -121,7 +107,7 @@ export function CustomImageSection5() {
                 className="relative flex flex-col justify-start rounded-xl bg-[#ee6a57] px-2 py-2 md:px-4 md:py-4"
               >
                 <div className="absolute flex items-center justify-center w-5 h-5 bg-white rounded-full -left-2 -top-2 md:h-8 md:w-8">
-                  <svg
+                  <svg aria-label="Check" role="img"
                     className="h-3 w-3 text-[#316704] md:h-5 md:w-5"
                     fill="currentColor"
                     viewBox="0 0 20 20"
@@ -148,7 +134,7 @@ export function CustomImageSection5() {
                 className="relative flex flex-col justify-start rounded-xl bg-[#ee6a57] px-2 py-2 md:px-4 md:py-4"
               >
                 <div className="absolute flex items-center justify-center w-5 h-5 bg-white rounded-full -left-2 -top-2 md:h-8 md:w-8">
-                  <svg
+                  <svg aria-label="Check" role="img"
                     className="h-3 w-3 text-[#316704] md:h-5 md:w-5"
                     fill="currentColor"
                     viewBox="0 0 20 20"
@@ -180,6 +166,7 @@ export function CustomImageSection5() {
           </div>
         </div>
       </div>
+      <img src={`${baseUrl}/assets/footer.png`} alt="Footer" className="max-w-7xl mx-auto absolute bottom-0 z-20" />
     </div>
   );
 }
