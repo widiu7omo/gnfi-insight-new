a2291c8aa0c73797d2dbb16e4a4fd257377edd27
Danar Widi <widiu7omo@gmail.com>
2024-12-09 18:14:14 +0800

    fix: responsive sec 1

--- trem-mrt-lrt-transportasi-rel-masa-depan-indonesia/src/components/custom-components/CustomImageTrem.tsx
+++ trem-mrt-lrt-transportasi-rel-masa-depan-indonesia/src/components/custom-components/CustomImageTrem.tsx
@@ -64,43 +64,66 @@ export function CustomImageTrem() {
       <div className="h-[100px] bg-gradient-to-b absolute w-full from-[#d0d1c3]" />
       <section
         id="section-1"
-        className="-mt-1 overflow-clip space-y-14 pb-10"
+        className="overflow-clip space-y-14"
       >
-        <div className="relative flex items-start justify-center max-w-4xl mx-auto z-10">
-          <div className="w-full bg-no-repeat bg-[cover,contain] xl:bg-[cover,auto] bg-[center_top] relative bg-blend-multiply">
-            <p className="text-3xl text-white font-extrabold px-4 md:px-6 w-fit py-2 bg-red-800">
-              Trem
-            </p>
-            <motion.p viewport={{ once: true }} className="text-left md:text-6xl text-xl text-red-900 font-extrabold pt-6" variants={paragraphVariant} initial="close" whileInView="open">
-              Transportasi Berbasis Rel Intrakota Pertama
-            </motion.p>
-            <motion.p viewport={{ once: true }} className="text-left text-2xl text-black font-extrabold pt-6" variants={paragraphVariant} initial="close" whileInView="open">
-              Trem adalah transportasi berbasis rel intrakota pertama di Indonesia yang sudah pernah ada di zaman Belanda
-            </motion.p>
-            <div className="relative flex items-center justify-between pt-8 z-10">
+        <div className="relative flex flex-col items-start justify-center z-10">
+          <div className="w-full bg-no-repeat bg-[cover,contain] xl:bg-[cover,auto] bg-[center_top] relative bg-blend-multiply max-w-4xl mx-auto ">
+            <div className="px-4 md:px-0">
+              <p className="text-3xl text-white font-extrabold px-4 md:px-6 w-fit py-2 bg-red-800">
+                Trem
+              </p>
+              <motion.p viewport={{ once: true }} className="text-left md:text-6xl text-4xl text-red-900 font-extrabold pt-6" variants={paragraphVariant} initial="close" whileInView="open">
+                Transportasi Berbasis Rel Intrakota Pertama
+              </motion.p>
+              <motion.p viewport={{ once: true }} className="text-left md:text-2xl text-xl text-black font-extrabold pt-6" variants={paragraphVariant} initial="close" whileInView="open">
+                Trem adalah transportasi berbasis rel intrakota pertama di Indonesia yang sudah pernah ada di zaman Belanda
+              </motion.p>
+            </div>
+            <div className="relative flex items-center flex-col md:flex-row justify-between pt-8 z-10">
               <motion.img viewport={{ once: true }}
                 initial="close"
-                whileInView="open"
+                whileInView="open" className="w-full md:w-auto"
                 src={`${baseUrl}/assets/1.1.png`}
                 alt="Trem"
               />
-              <div className="flex flex-col items-start justify-center flex-1 space-y-4">
+              <div className="flex flex-col items-start justify-center flex-1 space-y-4 px-4 md:px-0">
                 <div>
                   <div className="text-white w-fit bg-red-800 text-5xl font-bold px-2 py-1">Batavia</div>
                   <div className="bg-black px-2 py-1 text-2xl text-white">atau kini bernama Jakarta</div>
                 </div>
                 <div className="text-2xl">adalah kota Indonesia pertama yang mengoperasikan armada ini.</div>
-                <div className="italic text-lg pt-8">
+                <div className="italic text-lg pt-8 hidden md:block">
                   <p>Ilustrasi Foto:</p>
                   <p>Gatot Adri / Shutterstock</p>
                 </div>
               </div>
             </div>
-            <div style={{ backgroundImage: `url(${baseUrl}/assets/1.3.png)` }} className="-mt-16 bg-no-repeat bg-[cover,cover] bg-[center_bottom] bg-blend-multiply relative">
-              <div className="bg-[#281d1d] shadow-xl p-6 flex flex-col items-center justify-center space-y-12 mx-12 pt-12 relative">
-                <div className="text-3xl font-bold max-w-md text-center text-white">Daerah-daerah yang dilewati
+          </div>
+          <div style={{ backgroundImage: `url(${baseUrl}/assets/1.3.png)` }} className="md:mt-[-4.5rem] mt-4 w-full bg-no-repeat bg-[cover,cover] md:bg-[center_top] bg-blend-multiply relative">
+            <div className="max-w-4xl mx-auto">
+              <div className="bg-[#281d1d] shadow-xl p-6 flex flex-col items-center justify-center space-y-12 pt-8 md:pt-14 relative">
+                <div className="text-2xl md:text-3xl font-bold max-w-full w-full md:max-w-md text-center text-white">Daerah-daerah yang dilewati
                   di antaranya meliputi</div>
-                <div className="flex flex-wrap justify-center w-full relative">
+                <div className="w-full sm:hidden space-y-8 !mt-0 flex flex-col items-start justify-center px-4 pb-4 relative">
+                  <div className="w-[2px] h-[80%] bg-white absolute left-[2.9rem] top-[12%]" />
+                  <div className="flex-1 text-white py-1 flex items-center space-y-2 z-10">
+                    <img src={`${baseUrl}/assets/1.2.png`} alt="Station" className="bg-[#281d1d] mr-4 py-2" />
+                    <p className="text-xl font-medium">Pasar Ikan</p>
+                  </div>
+                  <div className="flex-1 text-white py-1 flex items-center space-y-2 z-10">
+                    <img src={`${baseUrl}/assets/1.2.png`} alt="Station" className="bg-[#281d1d] mr-4 py-2" />
+                    <p className="text-xl font-medium">Harmoni</p>
+                  </div>
+                  <div className="flex-1 text-white py-1 flex items-center space-y-2 z-10">
+                    <img src={`${baseUrl}/assets/1.2.png`} alt="Station" className="bg-[#281d1d] mr-4 py-2" />
+                    <p className="text-xl font-medium">Kramat</p>
+                  </div>
+                  <div className="flex-1 text-white py-1 flex items-center space-y-2 z-10">
+                    <img src={`${baseUrl}/assets/1.2.png`} alt="Station" className="bg-[#281d1d] mr-4 py-2" />
+                    <p className="text-xl font-medium">Meester Cornelis (Jatinegara)</p>
+                  </div>
+                </div>
+                <div className="w-full relative hidden sm:flex flex-wrap justify-center">
                   <div className="h-[2px] w-[80%] bg-white absolute top-[30%]" />
                   <div className="flex-1 text-white px-2 py-1 flex flex-col items-center space-y-2 z-10">
                     <img src={`${baseUrl}/assets/1.2.png`} alt="Station" className="bg-[#281d1d] px-4" />
@@ -120,10 +143,11 @@ export function CustomImageTrem() {
                   </div>
                 </div>
               </div>
-              <div className="h-[40rem]" />
             </div>
+            <div className="h-[30rem] md:h-[50rem]" />
           </div>
         </div>
+        <div className="absolute h-[20rem] bottom-[0rem] w-full bg-gradient-to-t from-[#d0d1c3] z-10" />
       </section>
     </div>
   );
