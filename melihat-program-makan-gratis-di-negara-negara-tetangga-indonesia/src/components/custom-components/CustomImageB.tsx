b8646dce9bce045cf21e58d3bf78629367636912
Danar Widi <widiu7omo@gmail.com>
2025-03-24 10:09:36 +0800

    section 2

--- melihat-program-makan-gratis-di-negara-negara-tetangga-indonesia/src/components/custom-components/CustomImageB.tsx
+++ melihat-program-makan-gratis-di-negara-negara-tetangga-indonesia/src/components/custom-components/CustomImageB.tsx
@@ -19,12 +19,12 @@ export function CustomImageB() {
   return (
     <div className="relative flex flex-col items-center overflow-hidden ">
       <div
-        className="relative flex h-full w-full justify-center bg-cover bg-top text-center pt-10 xl:pt-32"
+        className="relative flex h-full w-full justify-center bg-cover bg-center text-center pt-10 xl:pt-32"
         style={{ backgroundImage: `url(${baseUrl}/assets/2.0.jpg)` }}
       >
         <div className="absolute w-full h-[50%] bg-gradient-to-b from-[#51534d] top-0" />
-        <div className="z-10 max-w-4xl mx-auto">
-          <div className="relative space-y-2 inset-x-0 z-10 mb-4 text-center font-sora text-3xl font-bold text-white md:text-6xl flex flex-col justify-center items-center">
+        <div className="z-10 w-full">
+          <div className="max-w-4xl mx-auto relative space-y-2 inset-x-0 z-10 mb-4 text-center font-sora text-3xl font-bold text-white md:text-6xl flex flex-col justify-center items-center">
             <motion.h3
               viewport={{ once: true }}
               initial={{ opacity: 0, y: 20 }}
@@ -34,45 +34,75 @@ export function CustomImageB() {
               Program Makan Gratis di Malaysia, Sudah Ada sejak 1979
             </motion.h3>
           </div>
-          <div className="float-right">
-            <div className="w-[20rem] bg-blue-500 text-white relative font-sora p-8 rounded-2xl">
-              <img src={baseUrl + "/assets/2.1.png"} alt="Plate" className="absolute top-[-2rem] right-[-2rem] w-[10rem] z-10" />
+          <div className="flex max-w-4xl mx-auto z-10 relative">
+            <div className="basis-2/3" />
+            <div className="basis-1/3">
+              <div className="w-[20rem] bg-blue-500 text-white relative font-sora p-8 rounded-2xl">
+                <img src={baseUrl + "/assets/2.1.png"} alt="Plate" className="absolute top-[-2rem] right-[-2rem] w-[10rem] z-10" />
 
-              <div className="space-y-4 text-left">
-                <h6 className="pb-1 text-2xl font-semibold  mr-[4rem]">Rancangan
-                  Makanan
-                  Tambahan
-                  (RMT)</h6>
-                <p className="text-lg">
-                  Dimulai pada 1979,
-                  Pemerintah Malaysia membagikan makanan dan susu selama {" "}<b>190 hari</b> dalam setahun
-                </p>
-                <p className="text-lg">Didanai Kementerian Pendidikan. Untuk 2025, anggarannya{" "}
-                  <b>869 juta ringgit</b>
-                </p>
+                <div className="space-y-4 text-left">
+                  <h6 className="pb-1 text-2xl font-semibold  mr-[4rem]">Rancangan
+                    Makanan
+                    Tambahan
+                    (RMT)</h6>
+                  <p className="text-lg">
+                    Dimulai pada 1979,
+                    Pemerintah Malaysia membagikan makanan dan susu selama {" "}<b>190 hari</b> dalam setahun
+                  </p>
+                  <p className="text-lg">Didanai Kementerian Pendidikan. Untuk 2025, anggarannya{" "}
+                    <b>869 juta ringgit</b>
+                  </p>
+                </div>
+
               </div>
-
-            </div>
-            <div className="bg-black w-[20rem] text-white relative font-sora p-8 rounded-2xl -mt-6">
-              <img src={baseUrl + "/assets/2.2.png"} alt="Plate" className="absolute top-[-2rem] right-0 w-[10rem] z-10" />
-              <p className="text-lg text-left relative z-10">
-                <div>Penerima:</div>
-                <div className="mr-[5rem]">Siswa yang
-                  memenuhi
-                  salah satu dari
-                  kriteria berikut:</div>
-                <ol className="pl-5 list-decimal font-bold">
-                  <li>Pendapatan kepala
-                    keluarganya di bawah standar kemiskinan
-                  </li>
-                  <li>Orang Asli dan
-                    Orang Penan</li>
-                  <li>Berkebutuhan khusus</li>
-                </ol>
-              </p>
+              <div className="bg-black w-[20rem] text-white relative font-sora p-8 rounded-2xl -mt-6">
+                <img src={baseUrl + "/assets/2.2.png"} alt="Plate" className="absolute top-[-2rem] right-0 w-[10rem] z-10" />
+                <div className="text-lg text-left relative z-10">
+                  <div>Penerima:</div>
+                  <div className="mr-[5rem] pb-1.5">Siswa yang
+                    memenuhi
+                    salah satu dari
+                    kriteria berikut:</div>
+                  <ol className="pl-5 list-decimal font-bold">
+                    <li>Pendapatan kepala
+                      keluarganya di bawah standar kemiskinan
+                    </li>
+                    <li>Orang Asli dan
+                      Orang Penan</li>
+                    <li>Berkebutuhan khusus</li>
+                  </ol>
+                </div>
+              </div>
             </div>
           </div>
-          <img src={`${baseUrl}/assets/footer.png`} className="z-50 hidden sm:block relative bottom-0 h-fit w-fit mx-auto" />
+          <div className="bg-[#272727] h-full w-full -mt-5 pt-20 font-sora relative">
+            <div className="absolute top-[0rem] sm:top-[-4rem] lg:top-[-10rem] left-0 right-[-2rem] w-full">
+              <img src={baseUrl + '/assets/2.4.png'} className="object-contain w-full scale-[2] md:scale-100" alt="Flag" />
+            </div>
+            <div className="flex max-w-4xl mx-auto w-full flex-col lg:flex-row relative z-10 bg-[#272727]">
+              <div className="basis-1/2 flex flex-col p-8 items-start justify-start">
+                <h4 className="text-3xl text-white text-left w-full pb-6 font-semibold">Hidangan Berkhasiat di Sekolah (HiTS)</h4>
+                <ul className="text-left text-white text-lg space-y-2">
+                  <li className="flex space-x-2 items-start">
+                    <div className="w-0 h-0 border-t-[10px] mt-1.5 scale-[0.80] border-t-transparent border-b-[10px] border-b-transparent border-l-[20px] border-l-yellow-500"></div>
+                    <p>Dimulai sejak 2007 di Muar, Johor</p>
+                  </li>
+                  <li className="flex space-x-2 items-start">
+                    <div className="w-0 h-0 border-t-[10px] mt-1.5 scale-[0.80] border-t-transparent border-b-[10px] border-b-transparent border-l-[20px] border-l-yellow-500"></div>
+                    <p>Didanai pihak sekolah dan orang tua siswa</p>
+                  </li>
+                  <li className="flex space-x-2 items-start">
+                    <div className="w-0 h-0 border-t-[10px] mt-1.5 scale-[0.80] border-t-transparent border-b-[10px] border-b-transparent border-l-[20px] border-l-yellow-500"></div>
+                    <p>Tersedia pilihan setidaknya
+                      60 menu, lebih banyak dari
+                      RMT yang hanya 20 menu</p>
+                  </li>
+                </ul>
+              </div>
+              <img src={baseUrl + '/assets/2.3.jpg'} alt="Children" className="basis-1/2 w-full lg:w-1/2" />
+            </div>
+            <img src={`${baseUrl}/assets/footer.png`} className="z-50 hidden sm:block relative bottom-0 h-fit w-fit mx-auto" alt="Footer" />
+          </div>
         </div>
       </div>
     </div>
