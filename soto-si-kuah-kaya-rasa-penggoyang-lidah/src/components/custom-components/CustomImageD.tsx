16fe1765615e9a3bb40cca068c59bf47ed577c25
fknraini <fikana19@gmail.com>
2025-04-10 10:38:36 +0700

    section 4

--- soto-si-kuah-kaya-rasa-penggoyang-lidah/src/components/custom-components/CustomImageD.tsx
+++ soto-si-kuah-kaya-rasa-penggoyang-lidah/src/components/custom-components/CustomImageD.tsx
@@ -2,180 +2,260 @@
 import { baseUrl } from "@/constants/meta";
 import { motion } from "framer-motion";
 
-const SKANDAL = [
-  "Beberapa sekolah di Nakhon Ratchasima terdeteksi National Anti-Corruption Commission (NACC) menyajikan menu tak sesuai standar",
-  "Pimpinan sekolah di Chon Buri dilaporkan memanipulasi angka jumlah siswa penerima program",
-  "Sekolah Ban Tha Mai di Surat Thani menyajikan makanan minim gizi. Pimpinan sekolah lalu dicopot dari jabatannya",
-];
-
 export function CustomImageD() {
   return (
     <div className="relative flex flex-col items-center overflow-hidden">
       <div
-        className="relative w-full justify-center bg-cover bg-center pt-16 md:pt-20 lg:pb-4"
-        style={{ backgroundImage: `url(${baseUrl}/assets/3.6.png)` }}
+        className="relative w-full justify-center bg-cover bg-center pt-6 md:pt-12 lg:pb-4"
+        style={{ backgroundImage: `url(${baseUrl}/assets/4.5.png)` }}
       >
-        <div className="absolute bg-gradient-to-b from-[#028d85] top-0 left-0 h-[30%] w-full" />
-        <div className="mx-auto flex w-full flex-col items-center px-4 md:max-w-4xl md:flex-row md:items-start lg:px-0 relative z-10">
-          <div className="w-full lg:w-2/3">
-            <motion.h3
-              initial={{ opacity: 0, scale: 0.9, y: 10 }}
-              whileInView={{ opacity: 1, scale: 1, y: 0 }}
-              transition={{ duration: 0.4, ease: "easeOut" }}
-              viewport={{ once: true }}
-              className="mb-12 pb-8 text-left font-sora text-[1.75rem] font-bold leading-[1.2] text-white md:text-[48px] lg:text-[70px]"
-            >
-              Makan Gratis <br />
-              di Thailand yang Ternoda oleh Skandal
-            </motion.h3>
-            <motion.img
-              viewport={{ once: true }}
-              initial={{ opacity: 0, y: 50, scale: 0.8 }}
-              whileInView={{ opacity: 1, y: 0, scale: 1.1 }}
-              transition={{ duration: 0.8, ease: "backOut" }}
-              src={`${baseUrl}/assets/4.1.png`}
-              alt="Makan gratis"
-              className="relative z-30 -mt-12 h-auto w-full self-end md:-ml-4 md:ml-8"
-            />
-          </div>
-          <div className="z-40 w-full space-y-4 lg:w-1/3 lg:space-y-12">
-            <div className="z-20 w-full rounded-xl bg-white p-4 shadow-2xl md:w-[80%] md:p-6">
-              <motion.p
-                initial={{ opacity: 0, x: 30 }}
-                whileInView={{ opacity: 1, x: 0 }}
-                transition={{ duration: 0.8, ease: "easeOut" }}
-                viewport={{ once: true }}
-                className="text-sm text-gray-700 md:text-xl"
-              >
-                Pada 1992, Pemerintah Thailand meluncurkan pendanaan untuk Thai
-                School Lunch Program (Thai SLP)
-              </motion.p>
-            </div>
-            <div className="z-20 w-full rounded-xl bg-white p-4 shadow-2xl md:w-[80%] md:p-6">
-              <motion.p
-                initial={{ opacity: 0, x: 30 }}
-                whileInView={{ opacity: 1, x: 0 }}
-                transition={{ duration: 0.8, ease: "easeOut" }}
-                viewport={{ once: true }}
-                className="text-sm text-gray-700 md:text-xl"
-              >
-                Awalnya, setiap siswa menerima subsidi makanan 5 baht perhari
-                selama 200 hari dalam setahun, lalu pada 2021 dinaikkan menjadi
-                21 baht
-              </motion.p>
-            </div>
-          </div>
+        <div className="absolute left-0 right-0 top-0 z-10 flex justify-center">
+          <motion.img
+            viewport={{ once: true }}
+            initial={{ opacity: 0, y: -20, scale: 0.95 }}
+            whileInView={{ opacity: 1, y: 0, scale: 1 }}
+            transition={{ duration: 0.8, ease: "easeOut" }}
+            src={`${baseUrl}/assets/4.1.png`}
+            alt="Yellow Background"
+            className="min-h-[20rem] w-full object-cover md:h-full lg:max-h-[80%] lg:w-auto"
+          />
         </div>
-      </div>
-      <div
-        className="relative w-full justify-center bg-cover bg-center"
-        style={{ backgroundImage: `url(${baseUrl}/assets/4.2.png)` }}
-      >
-        <div className="mx-auto flex max-w-4xl flex-col items-center justify-center space-x-0 px-4 md:flex-row md:space-x-12 md:px-0">
-          <div className="relative flex h-[200px] w-full self-start md:h-[250px] md:w-1/2">
-            <motion.img
-              viewport={{ once: true }}
-              initial={{ opacity: 0, y: 50, scale: 0.8 }}
-              whileInView={{ opacity: 1, y: 0, scale: 1.1 }}
-              transition={{ duration: 0.8, ease: "backOut" }}
-              src={`${baseUrl}/assets/4.3.png`}
-              alt="Makan gratis"
-              className="z-30 ml-0 mt-2 h-[70%] w-auto md:-ml-12 md:mt-0 md:h-full md:w-auto"
-            />
-            <motion.img
-              viewport={{ once: true }}
-              initial={{ opacity: 0, y: 50, scale: 0.8 }}
-              whileInView={{ opacity: 1, y: 0, scale: 1.1 }}
-              transition={{ duration: 0.8, ease: "backOut" }}
-              src={`${baseUrl}/assets/4.4.png`}
-              alt="Makan gratis"
-              className="absolute left-[50%] top-2 z-30 h-auto w-1/2 md:top-4"
-            />
-          </div>
-          <div className="flex w-full py-6 md:w-1/2 md:py-0">
-            <motion.p
-              initial={{ opacity: 0, x: 50 }}
-              whileInView={{ opacity: 1, x: 0 }}
-              transition={{ duration: 0.8, ease: "easeOut" }}
-              viewport={{ once: true }}
-              className="text-lg leading-normal text-[#FFB433] md:text-[28px]"
-            >
-              Sekolah bertanggungjawab atas pelaksanaan Thai SLP untuk siswa
-              taman kanak-kanak hingga kelas 6
-            </motion.p>
-          </div>
-        </div>
-      </div>
-      <div
-        className="relative w-full justify-center bg-cover bg-center pb-0 pt-16 md:pt-20"
-        style={{ backgroundImage: `url(${baseUrl}/assets/4.5.jpg)` }}
-      >
-        <div className="mx-auto px-4 md:max-w-4xl lg:px-0 relative z-10">
-          <div className="flex w-full flex-col items-center space-x-0 md:flex-row md:space-x-4 lg:space-x-8">
-            <div className="flex h-full w-full flex-grow flex-col pb-20 md:pb-0 lg:w-4/5">
+
+        <div className="relative z-20 mx-auto w-full px-4 md:max-w-4xl lg:px-0">
+          <motion.h4
+            initial={{ opacity: 0, scale: 0.9, y: 10 }}
+            whileInView={{ opacity: 1, scale: 1, y: 0 }}
+            transition={{ duration: 0.4, ease: "easeOut" }}
+            viewport={{ once: true }}
+            className="mb-2 text-left font-sora text-xl font-bold text-[#2F2F2F] md:text-3xl lg:mb-4 lg:text-4xl"
+          >
+            Kecintaan Orang Indonesia
+          </motion.h4>
+          <div className="flex flex-row">
+            <div className="z-30 w-1/2">
               <motion.h3
                 initial={{ opacity: 0, scale: 0.9, y: 10 }}
                 whileInView={{ opacity: 1, scale: 1, y: 0 }}
                 transition={{ duration: 0.4, ease: "easeOut" }}
                 viewport={{ once: true }}
-                className="mb-8 text-left font-sora text-[1.75rem] font-bold leading-[1.2] text-[#009990] md:mb-12 md:text-[48px] lg:text-[70px]"
+                className="mb-2 text-left font-sora text-5xl font-extrabold text-white md:text-6xl lg:mb-4 lg:text-8xl"
+                style={{ textShadow: "2px 2px 6px rgba(0, 0, 0, 0.6)" }}
               >
-                Skandal yang <br />
-                menodai Thai SLP pada 2019:
+                Terhadap <br />
+                Soto
               </motion.h3>
-              <motion.div
-                className="space-y-8"
-                initial="hidden"
-                whileInView="visible"
+              <motion.p
+                initial={{ opacity: 0, scale: 0.9, y: 10 }}
+                whileInView={{ opacity: 1, scale: 1, y: 0 }}
+                transition={{ duration: 0.4, ease: "easeOut" }}
                 viewport={{ once: true }}
-                variants={{
-                  visible: {
-                    transition: {
-                      staggerChildren: 0.55,
-                    },
-                  },
-                }}
+                className="inline bg-white decoration-clone box-decoration-clone px-2 text-left font-sora text-lg text-[#2F2F2F] md:text-2xl md:leading-[2.5rem] lg:text-3xl lg:leading-[3rem]"
               >
-                {SKANDAL.map((item, index) => (
-                  <motion.div
-                    key={index}
-                    variants={{
-                      hidden: { opacity: 0, x: -40, scale: 0.9 },
-                      visible: {
-                        opacity: 1,
-                        x: 0,
-                        scale: 1,
-                        transition: { duration: 0.8, ease: "backOut" },
-                      },
-                    }}
-                    className="flex space-x-2"
-                  >
-                    <div className="mt-2 h-3 w-3 flex-shrink-0 rounded-full bg-[#009990] align-baseline" />
-                    <p className="text-left font-sora text-lg text-[#2A3335] md:text-2xl">
-                      {item}
-                    </p>
-                  </motion.div>
-                ))}
+                Penggemarnya soto berasal dari beragam kalangan, dari masyarakat
+                umum hingga selebritas ternama.
+              </motion.p>
+            </div>
+            <motion.img
+              viewport={{ once: true }}
+              initial={{ opacity: 0, y: 50, scale: 0.8 }}
+              whileInView={{ opacity: 1, y: 0, scale: 1.1 }}
+              transition={{ duration: 0.8, ease: "backOut" }}
+              src={`${baseUrl}/assets/4.2.png`}
+              alt="Soto"
+              className="z-20 -ml-[10%] mt-[25%] w-auto md:mt-0 md:h-auto md:w-[60%] lg:-ml-12 lg:h-[70%] lg:w-auto xl:h-full"
+            />
+          </div>
+          <div className="md:hidden">
+            <div className="flex items-end justify-center">
+              <motion.img
+                viewport={{ once: true }}
+                initial={{ opacity: 0, y: 50, scale: 0.8 }}
+                whileInView={{ opacity: 1, y: 0, scale: 1.1 }}
+                transition={{ duration: 0.8, ease: "easeOut" }}
+                src={`${baseUrl}/assets/4.3.png`}
+                alt="Yusril Fahriza"
+                className="z-30 h-auto w-[40%]"
+              />
+              <motion.div
+                viewport={{ once: true }}
+                initial={{ opacity: 0, x: -20 }}
+                whileInView={{ opacity: 1, x: 0 }}
+                transition={{ delay: 0.2, duration: 0.4 }}
+                className="ml-[5%]"
+              >
+                <p className="font-sera text-lg font-semibold text-[#2F2F2F] md:text-4xl">
+                  Komika Yogyakarta
+                </p>
+                <p className="font-sera text-2xl font-semibold text-[#FF521F] md:text-6xl">
+                  Yusril Fahriza
+                </p>
               </motion.div>
             </div>
-            <div className="h-full w-full md:w-2/5">
+            <div className="rounded-2xl bg-[#2F2F2F] px-4 py-4">
+              <motion.p
+                initial={{ opacity: 0, scale: 0.9, y: 20 }}
+                whileInView={{ opacity: 1, scale: 1, y: 0 }}
+                transition={{ duration: 0.4, ease: "easeOut" }}
+                viewport={{ once: true }}
+                className="font-sera text-base text-white"
+              >
+                Menunjukkan rasa cintanya terhadap soto lewat akun instagram{" "}
+                <span className="text-[#FF521F]">@demisotoimpian</span>. Rajin
+                menyambangi warung soto demi mendokumentasikan dirinya sedang
+                menyantap soto. Bukan di-review seperti akun "kode-kodean" ya!
+              </motion.p>
+            </div>
+          </div>
+          <div className="hidden md:block">
+            <div className="mx-auto flex flex-row items-end">
+              <div className="w-[70%]">
+                <motion.img
+                  viewport={{ once: true }}
+                  initial={{ opacity: 0, x: -50, scale: 0.8 }}
+                  whileInView={{ opacity: 1, x: 0, scale: 1.1 }}
+                  transition={{ duration: 0.8, ease: "backOut" }}
+                  src={`${baseUrl}/assets/4.3.png`}
+                  alt="Yusril Fahriza"
+                  className="z-30 h-auto max-h-[600px] w-auto object-contain"
+                />
+              </div>
+              <div className="flex w-[80%] flex-col">
+                <motion.div
+                  viewport={{ once: true }}
+                  initial={{ opacity: 0, x: -20 }}
+                  whileInView={{ opacity: 1, x: 0 }}
+                  transition={{ delay: 0.2, duration: 0.4 }}
+                  className="mb-2 space-y-1 md:mb-4 md:space-y-2"
+                >
+                  <p className="font-sera text-lg font-semibold text-[#2F2F2F] md:text-4xl">
+                    Komika Yogyakarta
+                  </p>
+                  <p className="font-sera text-2xl font-semibold text-[#FF521F] md:text-6xl">
+                    Yusril Fahriza
+                  </p>
+                </motion.div>
+                <div className="-mb-5 -ml-[15%] flex items-center rounded-[2rem] bg-[#2F2F2F] py-2 pl-[18%] pr-2 md:py-4 md:pl-[20%] md:pr-8">
+                  <motion.p
+                    initial={{ opacity: 0, scale: 0.9, x: 30 }}
+                    whileInView={{ opacity: 1, scale: 1, x: 0 }}
+                    transition={{ duration: 0.4, ease: "easeOut" }}
+                    viewport={{ once: true }}
+                    className="font-sera text-base text-white md:text-xl md:leading-[2rem] lg:text-2xl lg:leading-[2.5rem]"
+                  >
+                    Menunjukkan rasa cintanya terhadap soto lewat akun instagram{" "}
+                    <span className="text-[#FF521F]">@demisotoimpian</span>.
+                    Rajin menyambangi warung soto demi mendokumentasikan dirinya
+                    sedang menyantap soto. Bukan di-review seperti akun
+                    "kode-kodean" ya!
+                  </motion.p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="mb-14 mt-8 md:hidden">
+            <div className="flex items-end justify-center">
+              <motion.div
+                viewport={{ once: true }}
+                initial={{ opacity: 0, x: -20 }}
+                whileInView={{ opacity: 1, x: 0 }}
+                transition={{ delay: 0.2, duration: 0.4 }}
+                className="mr-[5%]"
+              >
+                <p className="font-sera text-right text-lg font-semibold text-[#2F2F2F]">
+                  Pemain naturalisasi Timnas Indonesia
+                </p>
+                <p className="font-sera text-right text-2xl font-semibold text-[#FF521F]">
+                  Mees Hilgers
+                </p>
+              </motion.div>
               <motion.img
                 viewport={{ once: true }}
                 initial={{ opacity: 0, y: 50, scale: 0.8 }}
                 whileInView={{ opacity: 1, y: 0, scale: 1.1 }}
                 transition={{ duration: 0.8, ease: "backOut" }}
-                src={`${baseUrl}/assets/4.6.png`}
-                alt="Makan gratis"
-                className="z-50 h-full w-full object-cover"
+                src={`${baseUrl}/assets/4.4.png`}
+                alt="Mees Hilgers"
+                className="z-30 h-auto w-[40%]"
               />
             </div>
+            <div className="rounded-2xl bg-[#2F2F2F] px-4 py-4">
+              <motion.p
+                initial={{ opacity: 0, scale: 0.9, y: 10 }}
+                whileInView={{ opacity: 1, scale: 1, y: 0 }}
+                transition={{ duration: 0.4, ease: "easeOut" }}
+                viewport={{ once: true }}
+                className="font-sera text-base text-white"
+              >
+                Menyukai soto dan malahan bisa meraciknya sendiri sesuai selera.
+                <br />
+                {""}
+                <br />
+                "Soto ayam adalah favorit saya. Itu adalah sup khas dari
+                Indonesia yang sangat lezat, terutama jika ibu saya yang
+                membuatnya. Saya juga bisa membuatnya sendiri. Biasanya, ibu
+                saya membangunkan saya untuk makan soto ayam yang telah dia
+                buat," kata Hilgers{" "}
+              </motion.p>
+            </div>
           </div>
-          <img
-            src={`${baseUrl}/assets/footer.png`}
-            className="mx-auto hidden w-full sm:block invert"
-          />
+          <div className="mb-32 mt-48 hidden md:block">
+            <div className="mx-auto -mt-32 flex flex-row items-end">
+              <div className="-mr-[15%] flex w-[90%] flex-col">
+                <motion.div
+                  viewport={{ once: true }}
+                  initial={{ opacity: 0, x: 20 }}
+                  whileInView={{ opacity: 1, x: 0 }}
+                  transition={{ delay: 0.2, duration: 0.4 }}
+                  className="mb-6 space-y-2"
+                >
+                  <p className="font-sera text-end text-lg font-semibold text-[#2F2F2F] md:text-4xl">
+                    Pemain naturalisasi Timnas Indonesia
+                  </p>
+                  <p className="font-sera text-end text-2xl font-semibold text-[#FF521F] md:text-6xl">
+                    Mees Hilgers
+                  </p>
+                </motion.div>
+                <div className="-mb-[4.5%] flex items-center rounded-[2rem] bg-[#2F2F2F] py-2 pl-2 pr-[18%] md:py-4 md:pl-8 md:pr-[18%] lg:pr-[16%]">
+                  <motion.p
+                    initial={{ opacity: 0, scale: 0.9, x: -30 }}
+                    whileInView={{ opacity: 1, scale: 1, x: 0 }}
+                    transition={{ duration: 0.4, ease: "easeOut" }}
+                    viewport={{ once: true }}
+                    className="font-sera text-base text-white md:text-xl md:leading-[2rem] lg:text-2xl lg:leading-[2.5rem]"
+                  >
+                    Menyukai soto dan malahan bisa meraciknya sendiri sesuai
+                    selera.
+                    <br />
+                    {""}
+                    <br />
+                    "Soto ayam adalah favorit saya. Itu adalah sup khas dari
+                    Indonesia yang sangat lezat, terutama jika ibu saya yang
+                    membuatnya. Saya juga bisa membuatnya sendiri. Biasanya, ibu
+                    saya membangunkan saya untuk makan soto ayam yang telah dia
+                    buat," kata Hilgers{" "}
+                  </motion.p>
+                </div>
+              </div>
+              <div className="-mr-[20%] w-[80%]">
+                <motion.img
+                  viewport={{ once: true }}
+                  initial={{ opacity: 0, x: 50, scale: 0.8 }}
+                  whileInView={{ opacity: 1, x: 0, scale: 1.1 }}
+                  transition={{ duration: 0.8, ease: "backOut" }}
+                  src={`${baseUrl}/assets/4.4.png`}
+                  alt="Mees Hilgers"
+                  className="z-30 h-auto max-h-[600px] w-auto object-contain"
+                />
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="absolute bg-gradient-to-t from-white bottom-0 left-0 h-[30%] w-full z-0" />
+      </div>
+      <div className="absolute bottom-0 z-50 mx-auto max-w-4xl">
+        <img
+          src={`${baseUrl}/assets/footer.png`}
+          className="z-50 mx-auto hidden w-full invert sm:block"
+        />
       </div>
     </div>
   );
