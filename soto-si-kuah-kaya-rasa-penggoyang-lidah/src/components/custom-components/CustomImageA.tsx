bd2e7b31694f645345edf11ef4f03fe955e1bba4
Danar Widi <widiu7omo@gmail.com>
2025-04-11 18:46:45 +0800

    WIP

--- soto-si-kuah-kaya-rasa-penggoyang-lidah/src/components/custom-components/CustomImageA.tsx
+++ soto-si-kuah-kaya-rasa-penggoyang-lidah/src/components/custom-components/CustomImageA.tsx
@@ -9,119 +9,63 @@ export function CustomImageA() {
   const inView = useInView(ref)
 
   const canAnimate = useCanAnimate()
-  const problems = [
-    'Pemotongan biaya makanan perporsi dari Rp15 ribu menjadi Rp10 ribu',
-    'Keracunan massal di Sukoharjo',
-    'Penemuan lauk daging ayam mentah di Kupang',
-    'Aksi penolakan di Papua Tengah',
-    'Wacana penggunaan susu ikan',
-  ]
+
   return (
-    <div ref={ref} className="relative overflow-clip font-sora py-14 bg-no-repeat bg-cover bg-top" style={{ backgroundImage: `url(${baseUrl}/assets/1.0.1.png)`, backgroundColor: "black" }}>
-      <div className="absolute left-0 w-full bottom-0 h-[20%] bg-[#4f2911] z-0"></div>
+    <div ref={ref} className="relative overflow-clip font-sora py-14 bg-no-repeat bg-cover" style={{ backgroundImage: `url(${baseUrl}/assets/1.0.2.png)`, backgroundColor: 'black' }}>
+      <div className="absolute left-0 w-full bottom-0 h-[20%] z-0 bg-gradient-to-t from-black"></div>
       <div className="absolute left-0 w-full top-0 h-full bg-black/30 z-0"></div>
       <div className="max-w-4xl mx-auto relative z-10 pt-0 lg:pt-[6rem] pb-20">
-        <motion.h4 initial={{ opacity: 0, y: -20 }} whileInView={{ opacity: 1, y: 0 }}
-          transition={{ duration: .5 }}
-          viewport={{ once: true }}
-          className="font-sora w-full text-4xl md:text-6xl xl:text-7xl font-bold !leading-tight tracking-wide text-white text-center">Makan Bergizi Gratis "Gebrakan" Baru Indonesia
-        </motion.h4>
-        <div className="h-[10rem] lg:h-[50rem]"></div>
-        <div className="flex flex-col md:flex-row flex-wrap">
-          <div className="relative basis-1/2 p-2">
-            <motion.img
+        <div className="h-[60rem]"></div>
+        <div className="flex">
+          <div className="flex-1">
+            <motion.h4 initial={{ opacity: 0, y: -20 }} whileInView={{ opacity: 1, y: 0 }}
+              transition={{ duration: .5 }}
               viewport={{ once: true }}
-              initial={{ opacity: 0, y: 20 }}
-              whileInView={{ opacity: 1, y: 0 }}
-              transition={{ delay: 0, duration: 0.6 }}
-              src={baseUrl + '/assets/1.2.png'} alt="Capres" className="absolute top-[-7rem] w-[10rem] left-[16px]" />
-            <motion.div
-              viewport={{ once: true }}
-              initial={{ opacity: 0, y: -20 }}
-              whileInView={{ opacity: 1, y: 0 }}
-              transition={{ delay: 0, duration: 0.6 }}
-              className="bg-[#352925] p-4 xl:p-6 rounded-xl relative ">
-              <p className="text-white text-lg lg:text-2xl tracking-wide leading-snug">Merupakan <span className="font-bold">janji politik</span>
-                {' '}Prabowo Subianto- Gibran Rakabuming di Pilpres
-                2024</p>
-            </motion.div>
+              className="font-sora w-full text-4xl md:text-6xl xl:text-7xl font-bold !leading-tight tracking-wide text-white text-left">
+              Asal
+              Muasal
+              Soto
+            </motion.h4>
+            <div>
+              <div className="flex items-center gap-2">
+                <img src={`${baseUrl}/assets/1.2.png`} alt="" className="size-16" />
+                <p className="font-sora text-2xl text-white">Menurut
+                  Aji K. Bromokusumo</p>
+              </div>
+              <p className="text-white text-2xl font-sora">Orang-orang Belanda dan
+                Tiongkok yang kampung halamannya negara empat musim biasa menyantap makanan berwujud sup untuk menghangatkan
+                tubuh. Dari situ, kuliner kuah-kuahan pun hadir di nusantara</p>
+            </div>
           </div>
-          <div className="relative basis-1/2 p-2">
-            <motion.img
-              viewport={{ once: true }}
-              initial={{ opacity: 0, scale: 0 }}
-              whileInView={{ opacity: 1, scale: 1 }}
-              transition={{ delay: 0.1, duration: 0.6 }}
-              src={baseUrl + '/assets/1.3.png'} alt="Money" className="absolute top-[-3rem] h-[8rem] lg:h-[10rem] right-0 z-10" />
-            <motion.div
-              viewport={{ once: true }}
-              initial={{ opacity: 0, y: -20 }}
-              whileInView={{ opacity: 1, y: 0 }}
-              transition={{ delay: 0.1, duration: 0.6 }}
-              className="bg-[#352925] p-4 xl:p-6 rounded-xl relative pr-[5rem]">
-              <p className="text-white text-lg lg:text-2xl tracking-wide leading-snug">Anggaran sebesar <span className="font-bold">Rp171 triliun</span> untuk 2025</p>
-            </motion.div>
-          </div>
-          <div className="relative basis-1/2 p-2">
-            <motion.img
-              viewport={{ once: true }}
-              initial={{ opacity: 0, scale: 0 }}
-              whileInView={{ opacity: 1, scale: 1 }}
-              transition={{ delay: 0.2, duration: 0.6 }}
-              src={baseUrl + '/assets/1.4.png'} alt="Rocket" className="absolute top-[-2rem] w-[5rem] lg:w-[8rem] right-8 lg:right-0 z-10" />
-            <motion.div
-              viewport={{ once: true }}
-              initial={{ opacity: 0, y: -20 }}
-              whileInView={{ opacity: 1, y: 0 }}
-              transition={{ delay: 0.2, duration: 0.6 }}
-              className="bg-[#352925] p-4 xl:p-6 rounded-xl relative">
-              <p className="text-white text-lg lg:text-2xl tracking-wide leading-snug pr-[5rem]">Mulai digulirkan sejak Senin<span className="font-bold">(6/1/2025)</span></p>
-            </motion.div>
-          </div>
-          <div className="relative basis-1/2 p-2 mt-0 md:-mt-6 lg:-mt-8 2xl:-mt-18">
-            <motion.img
-              viewport={{ once: true }}
-              initial={{ opacity: 0, scale: 0 }}
-              whileInView={{ opacity: 1, scale: 1 }}
-              transition={{ delay: 0.3, duration: 0.6 }}
-              src={baseUrl + '/assets/1.5.png'} alt="Arrow" className="absolute top-0 bottom-0 lg:bottom-0 lg:top-[-3rem] w-[6rem] lg:w-[10rem] right-0 lg:right-[-3rem] z-10" />
-            <motion.div
-              viewport={{ once: true }}
-              initial={{ opacity: 0, y: -20 }}
-              whileInView={{ opacity: 1, y: 0 }}
-              transition={{ delay: 0.3, duration: 0.6 }}
-              className="bg-[#352925] p-4 xl:p-6 rounded-xl relative ">
-              <p className="text-white text-lg lg:text-2xl tracking-wide leading-snug pr-[5rem]">Diklaim menjangkau
-                38 provinsi dengan
-                lebih dari  <span className="font-bold">2 juta penerima</span>
-                {' '}per akhir Februari 2025</p>
-            </motion.div>
+          <div className="flex-1">
+            <img src={`${baseUrl}/assets/1.1.png`} alt="" className="" />
           </div>
         </div>
       </div>
-      <div className="bg-[#4f2911] pt-16">
-        <div className="flex flex-col max-w-4xl mx-auto relative px-6 pb-10">
-          <motion.div
-            viewport={{ once: true }}
-            initial={{ opacity: 0, x: -20 }}
-            whileInView={{ opacity: 1, x: 0 }}
-            transition={{ delay: 0, duration: 0.6 }}
-            className="absolute px-8 left-0 md:left-[8px] shadow-lg py-4 -top-[6rem] text-2xl font-bold text-white rounded-r-full bg-[#8e4c1f]">Beberapa Polemik MBG</motion.div>
-          <div className="flex flex-col md:flex-row flex-wrap relative z-10">
-            {problems.map((problem, index) => (
-              <motion.div
-                viewport={{ once: true }}
-                initial={{ opacity: 0, y: -20 }}
-                whileInView={{ opacity: 1, y: 0 }}
-                transition={{ delay: index * 0.1, duration: 0.6 }}
-                key={index} className={`flex basis-1/2 flex-row items-start space-x-2 text-white text-xl lg:px-4 py-2 h-fit ${index == problems.length - 2 ? 'lg:-mt-12' : ''}`}>
-                <div className="min-w-4 min-h-4 bg-[#e8b795] rounded-full mt-1.5"></div>
-                <p>{problem}</p>
-              </motion.div>
-            ))}
+      <div className="relative flex flex-col items-center justify-center">
+        <img src={`${baseUrl}/assets/1.3.1.png`} alt="" className="max-w-7xl" />
+        <div className="absolute top-[15%] max-w-4xl flex space-x-8">
+          <div className="flex-1 space-y-8">
+            <div className="font-sora text-5xl font-bold max-w-[90%] leading-tight">
+              Lalu bagaimana bisa namanya jadi <span className="rounded-xl px-2 py-0 bg-[#e19958]">soto?</span>
+            </div>
+            <img src={`${baseUrl}/assets/1.5.png`} alt="" className="" />
           </div>
+          <div className="flex-1 flex flex-col space-y-7">
+            <div className="text-3xl font-sora font-semibold pt-4 pl-24">Ada dua pendapat</div>
+            <div className="flex items-center space-x-4">
+              <img src={`${baseUrl}/assets/1.3.png`} alt="" className="size-20" />
+              <div className="min-h-2 min-w-2 bg-orange-500 rounded-full" />
+              <p className="font-sora text-lg">Pakar Kajian Asia terkemuka, Denys Lombard, menyebut nama soto berasal dari alias <i>cao du</i>, atau dalam dialek Hokki</p>
+            </div>
+            <div className="flex items-center space-x-4">
+              <img src={`${baseUrl}/assets/1.4.png`} alt="" className="size-20" />
+              <div className="min-h-2 min-w-2 bg-orange-500 rounded-full" />
+              <p className="font-sora text-lg">Russel Jones dalam <b>Loanwords in Indonesian-Malay</b> berpendapat soto berasal dari alias <i>shao du</i> yang dibaca <i>sio to</i> dengan dialek Hokkien</p>
+            </div>
+          </div>
+
         </div>
-        <img src={`${baseUrl}/assets/1.6.png`} alt="" className="absolute bottom-0 right-0 w-[50%] lg:w-[45%] z-0" />
       </div>
       <img src={`${baseUrl}/assets/footer.png`} className="z-50 hidden sm:block relative bottom-0 h-full w-fit mx-auto" />
     </div >
--- soto-si-kuah-kaya-rasa-penggoyang-lidah/src/components/custom-components/CustomImageB.tsx
+++ soto-si-kuah-kaya-rasa-penggoyang-lidah/src/components/custom-components/CustomImageB.tsx
@@ -19,139 +19,51 @@ export function CustomImageB() {
   return (
     <div className="relative flex flex-col items-center overflow-hidden ">
       <div
-        className="relative flex h-full w-full justify-center bg-cover bg-center text-center pt-10 xl:pt-32"
-        style={{ backgroundImage: `url(${baseUrl}/assets/2.0.jpg)` }}
+        className="relative flex h-full w-full justify-center bg-cover bg-no-repeat text-center pt-10 xl:pt-32 bg-[center_top_-20rem]"
+        style={{ backgroundImage: `url(${baseUrl}/assets/2.0.1.png)` }}
       >
-        <div className="absolute w-full h-[50%] bg-gradient-to-b from-[#51534d] top-0" />
+        <div className="absolute w-full h-[50%] bg-gradient-to-b from-[#f0eff0] top-0" />
         <div className="z-10 w-full">
-          <div className="max-w-4xl mx-auto relative space-y-2 inset-x-0 z-10 mb-4 text-center font-sora text-3xl font-bold text-white md:text-6xl flex flex-col justify-center items-center">
+          <div className="max-w-4xl mx-auto relative space-y-6 inset-x-0 z-10 mb-4 text-center font-sora text-neutral-950 flex flex-col justify-center items-center">
             <motion.h3
               viewport={{ once: true }}
               initial={{ opacity: 0, y: 20 }}
               whileInView={{ opacity: 1, y: 0 }}
               transition={{ delay: 0, duration: 0.6 }}
-              className="px-4 py-2 rounded-xl break-words w-fit">
-              Program Makan Gratis di Malaysia, Sudah Ada sejak 1979
+              className="px-4 py-2 rounded-xl break-words w-fit text-3xl md:text-7xl font-bold">
+              Bukan Sekadar Kuliner Kuah-kuahan Biasa
             </motion.h3>
+            <motion.p className="text-3xl xl:px-3">
+              Soto sendiri pada dasarnya menggunakan bumbu utama sederhana. Akan tetapi, ketika memasuki wilayah-wilayah Indonesia, soto menjadi kaya dengan bumbu seperti:
+            </motion.p>
           </div>
-          <div className="flex max-w-4xl mx-auto z-10 relative">
-            <div className="basis-2/3" />
-            <div className="basis-1/3">
-              <motion.div
-                viewport={{ once: true }}
-                initial={{ opacity: 0, x: 20 }}
-                whileInView={{ opacity: 1, x: 0 }}
-                transition={{ delay: 0, duration: 0.6 }}
-                className="w-[20rem] bg-blue-500 text-white relative font-sora p-8 rounded-2xl">
-                <motion.img
-                  viewport={{ once: true }}
-                  initial={{ opacity: 0, scale: 0 }}
-                  whileInView={{ opacity: 1, scale: 1 }}
-                  transition={{ delay: 0.1, duration: 0.5 }}
-                  src={baseUrl + "/assets/2.1.png"} alt="Plate" className="absolute top-[-2rem] right-[-2rem] w-[10rem] z-10" />
-
-                <div className="space-y-4 text-left">
-                  <h6 className="pb-1 text-2xl font-semibold  mr-[4rem]">Rancangan
-                    Makanan
-                    Tambahan
-                    (RMT)</h6>
-                  <p className="text-lg">
-                    Dimulai pada 1979,
-                    Pemerintah Malaysia membagikan makanan dan susu selama {" "}<b>190 hari</b> dalam setahun
-                  </p>
-                  <p className="text-lg">Didanai Kementerian Pendidikan. Untuk 2025, anggarannya{" "}
-                    <b>869 juta ringgit</b>
-                  </p>
-                </div>
-
-              </motion.div>
-              <motion.div
-                viewport={{ once: true }}
-                initial={{ opacity: 0, x: 20 }}
-                whileInView={{ opacity: 1, x: 0 }}
-                transition={{ delay: 0.2, duration: 0.4 }}
-                className="bg-black w-[20rem] text-white relative font-sora p-8 rounded-2xl -mt-6">
-                <motion.img
-                  viewport={{ once: true }}
-                  initial={{ opacity: 0, x: 20 }}
-                  whileInView={{ opacity: 1, x: 0 }}
-                  transition={{ delay: 0.3, duration: 0.6 }}
-                  src={baseUrl + "/assets/2.2.png"} alt="Plate" className="absolute top-[-2rem] right-0 w-[10rem] z-10" />
-                <div className="text-lg text-left relative z-10">
-                  <div>Penerima:</div>
-                  <div className="mr-[5rem] pb-1.5">Siswa yang
-                    memenuhi
-                    salah satu dari
-                    kriteria berikut:</div>
-                  <ol className="pl-5 list-decimal font-bold">
-                    <li>Pendapatan kepala
-                      keluarganya di bawah standar kemiskinan
-                    </li>
-                    <li>Orang Asli dan
-                      Orang Penan</li>
-                    <li>Berkebutuhan khusus</li>
-                  </ol>
-                </div>
-              </motion.div>
-            </div>
+          <div className="h-[70rem] relative flex flex-col items-center justify-center max-w-4xl mx-auto">
+            <img src={`${baseUrl}/assets/2.1.png`} alt="" className="absolute top-0" />
+            <img src={`${baseUrl}/assets/2.2.png`} alt="" className="absolute top-[20%] left-[-4rem]" />
+            <img src={`${baseUrl}/assets/2.3.png`} alt="Lengkuas" className="absolute top-[30%] right-5" />
+            <img src={`${baseUrl}/assets/2.4.png`} alt="" className="absolute bottom-[20%] left-0" />
+            <img src={`${baseUrl}/assets/2.5.png`} alt="" className="absolute bottom-[25%] right-0" />
           </div>
-          <div className="bg-[#272727] h-full w-full -mt-5 pt-20 font-sora relative">
-            <div className="absolute top-[0rem] sm:top-[-4rem] lg:top-[-10rem] left-0 right-[-2rem] w-full">
+          <div className="flex flex-col max-w-7xl mx-auto z-10 relative">
+            <div className="h-[20rem]" />
+            <div className="relative flex items-center justify-center">
               <motion.img
                 viewport={{ once: true }}
-                initial={{ opacity: 0 }}
-                whileInView={{ opacity: 1 }}
+                initial={{ opacity: 0, x: 20 }}
+                whileInView={{ opacity: 1, x: 0 }}
                 transition={{ delay: 0, duration: 0.6 }}
-                src={baseUrl + '/assets/2.4.png'} className="object-contain w-full scale-[2] md:scale-100" alt="Flag" />
-            </div>
-            <div className="flex max-w-4xl mx-auto w-full flex-col lg:flex-row relative z-10 bg-[#272727]">
-              <div className="basis-1/2 flex flex-col p-8 items-start justify-start">
-                <motion.h4
-                  viewport={{ once: true }}
-                  initial={{ opacity: 0, y: -20 }}
-                  whileInView={{ opacity: 1, y: 0 }}
-                  transition={{ delay: 0, duration: 0.6 }}
-                  className="text-3xl text-white text-left w-full pb-6 font-semibold">Hidangan Berkhasiat di Sekolah (HiTS)</motion.h4>
-                <ul className="text-left text-white text-lg space-y-2">
-                  <motion.li
-                    viewport={{ once: true }}
-                    initial={{ opacity: 0, y: -20 }}
-                    whileInView={{ opacity: 1, y: 0 }}
-                    transition={{ delay: 0, duration: 0.6 }}
-                    className="flex space-x-2 items-start">
-                    <div className="w-0 h-0 border-t-[10px] mt-1.5 scale-[0.80] border-t-transparent border-b-[10px] border-b-transparent border-l-[20px] border-l-yellow-500"></div>
-                    <p>Dimulai sejak 2007 di Muar, Johor</p>
-                  </motion.li>
-                  <motion.li
-                    viewport={{ once: true }}
-                    initial={{ opacity: 0, y: -20 }}
-                    whileInView={{ opacity: 1, y: 0 }}
-                    transition={{ delay: 0.1, duration: 0.5 }}
-                    className="flex space-x-2 items-start">
-                    <div className="w-0 h-0 border-t-[10px] mt-1.5 scale-[0.80] border-t-transparent border-b-[10px] border-b-transparent border-l-[20px] border-l-yellow-500"></div>
-                    <p>Didanai pihak sekolah dan orang tua siswa</p>
-                  </motion.li>
-                  <motion.li
-                    viewport={{ once: true }}
-                    initial={{ opacity: 0, y: -20 }}
-                    whileInView={{ opacity: 1, y: 0 }}
-                    transition={{ delay: 0.2, duration: 0.4 }}
-                    className="flex space-x-2 items-start">
-                    <div className="w-0 h-0 border-t-[10px] mt-1.5 scale-[0.80] border-t-transparent border-b-[10px] border-b-transparent border-l-[20px] border-l-yellow-500"></div>
-                    <p>Tersedia pilihan setidaknya
-                      60 menu, lebih banyak dari
-                      RMT yang hanya 20 menu</p>
-                  </motion.li>
-                </ul>
+                src={`${baseUrl}/assets/2.6.png`} className="w-full" alt="Chef" />
+              <div className="absolute top-[23%] max-w-sm">
+                <div className="absolute top-1 max-w-sm left-[-20px] w-0 h-0 border-t-8 border-b-8 border-l-8 border-t-transparent border-b-transparent border-l-orange-500" />
+                <p className="text-left font-sora text-white font-medium text-2xl relative">Pakar kuliner nusantara, <br />
+                  <b>William Wongso</b> mengatakan jika
+                  menyebut soto wajib menambahkan
+                  asal daerahnya, misalnya: soto Kudus,
+                  soto Banjar, soto Betawi, dan masih
+                  banyak lagi</p>
               </div>
-              <motion.img
-                viewport={{ once: true }}
-                initial={{ opacity: 0, y: -20 }}
-                whileInView={{ opacity: 1, y: 0 }}
-                transition={{ delay: 0.2, duration: 0.4 }}
-                src={baseUrl + '/assets/2.3.jpg'} alt="Children" className="basis-1/2 w-full lg:w-1/2" />
             </div>
-            <img src={`${baseUrl}/assets/footer.png`} className="z-50 hidden sm:block relative bottom-0 h-fit w-fit mx-auto" alt="Footer" />
+            <img src={`${baseUrl}/assets/footer.png`} className="z-50 absolute hidden sm:block bottom-0 h-fit w-fit mx-auto" alt="Footer" />
           </div>
         </div>
       </div>
