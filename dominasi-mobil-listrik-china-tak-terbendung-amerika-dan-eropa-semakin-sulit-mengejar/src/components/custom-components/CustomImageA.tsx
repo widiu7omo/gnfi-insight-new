9525d9ee64a6579c9086ee75710fc302cf37abc4
Danar Widi <widiu7omo@gmail.com>
2025-04-17 20:40:22 +0800

    responsive

--- dominasi-mobil-listrik-china-tak-terbendung-amerika-dan-eropa-semakin-sulit-mengejar/src/components/custom-components/CustomImageA.tsx
+++ dominasi-mobil-listrik-china-tak-terbendung-amerika-dan-eropa-semakin-sulit-mengejar/src/components/custom-components/CustomImageA.tsx
@@ -17,114 +17,50 @@ export function CustomImageA() {
     'Wacana penggunaan susu ikan',
   ]
   return (
-    <div ref={ref} className="relative overflow-clip font-sora py-14 bg-no-repeat bg-cover bg-top" style={{ backgroundImage: `url(${baseUrl}/assets/1.0.1.png)`, backgroundColor: "#d0d1c3" }}>
-      <div className="absolute left-0 w-full bottom-0 h-[20%] bg-[#4f2911] z-0"></div>
-      <div className="absolute left-0 w-full top-0 h-full bg-black/30 z-0"></div>
-      <div className="max-w-4xl mx-auto relative z-10 pt-0 lg:pt-[6rem] pb-20">
-        <motion.h4 initial={{ opacity: 0, y: -20 }} whileInView={{ opacity: 1, y: 0 }}
-          transition={{ duration: .5 }}
-          viewport={{ once: true }}
-          className="font-sora w-full text-4xl md:text-6xl xl:text-7xl font-bold !leading-tight tracking-wide text-white text-center">Makan Bergizi Gratis "Gebrakan" Baru Indonesia
-        </motion.h4>
-        <div className="h-[10rem] lg:h-[50rem]"></div>
-        <div className="flex flex-col md:flex-row flex-wrap">
-          <div className="relative basis-1/2 p-2">
-            <motion.img
-              viewport={{ once: true }}
-              initial={{ opacity: 0, y: 20 }}
-              whileInView={{ opacity: 1, y: 0 }}
-              transition={{ delay: 0, duration: 0.6 }}
-              src={baseUrl + '/assets/1.2.png'} alt="Capres" className="absolute top-[-7rem] w-[10rem] left-[16px]" />
-            <motion.div
-              viewport={{ once: true }}
-              initial={{ opacity: 0, y: -20 }}
-              whileInView={{ opacity: 1, y: 0 }}
-              transition={{ delay: 0, duration: 0.6 }}
-              className="bg-[#352925] p-4 xl:p-6 rounded-xl relative ">
-              <p className="text-white text-lg lg:text-2xl tracking-wide leading-snug">Merupakan <span className="font-bold">janji politik</span>
-                {' '}Prabowo Subianto- Gibran Rakabuming di Pilpres
-                2024</p>
-            </motion.div>
-          </div>
-          <div className="relative basis-1/2 p-2">
-            <motion.img
-              viewport={{ once: true }}
-              initial={{ opacity: 0, scale: 0 }}
-              whileInView={{ opacity: 1, scale: 1 }}
-              transition={{ delay: 0.1, duration: 0.6 }}
-              src={baseUrl + '/assets/1.3.png'} alt="Money" className="absolute top-[-3rem] h-[8rem] lg:h-[10rem] right-0 z-10" />
-            <motion.div
-              viewport={{ once: true }}
-              initial={{ opacity: 0, y: -20 }}
-              whileInView={{ opacity: 1, y: 0 }}
-              transition={{ delay: 0.1, duration: 0.6 }}
-              className="bg-[#352925] p-4 xl:p-6 rounded-xl relative pr-[5rem]">
-              <p className="text-white text-lg lg:text-2xl tracking-wide leading-snug">Anggaran sebesar <span className="font-bold">Rp171 triliun</span> untuk 2025</p>
-            </motion.div>
-          </div>
-          <div className="relative basis-1/2 p-2">
-            <motion.img
-              viewport={{ once: true }}
-              initial={{ opacity: 0, scale: 0 }}
-              whileInView={{ opacity: 1, scale: 1 }}
-              transition={{ delay: 0.2, duration: 0.6 }}
-              src={baseUrl + '/assets/1.4.png'} alt="Rocket" className="absolute top-[-2rem] w-[5rem] lg:w-[8rem] right-8 lg:right-0 z-10" />
-            <motion.div
-              viewport={{ once: true }}
-              initial={{ opacity: 0, y: -20 }}
-              whileInView={{ opacity: 1, y: 0 }}
-              transition={{ delay: 0.2, duration: 0.6 }}
-              className="bg-[#352925] p-4 xl:p-6 rounded-xl relative">
-              <p className="text-white text-lg lg:text-2xl tracking-wide leading-snug pr-[5rem]">Mulai digulirkan sejak Senin<span className="font-bold">(6/1/2025)</span></p>
-            </motion.div>
-          </div>
-          <div className="relative basis-1/2 p-2 mt-0 md:-mt-6 lg:-mt-8 2xl:-mt-18">
-            <motion.img
-              viewport={{ once: true }}
-              initial={{ opacity: 0, scale: 0 }}
-              whileInView={{ opacity: 1, scale: 1 }}
-              transition={{ delay: 0.3, duration: 0.6 }}
-              src={baseUrl + '/assets/1.5.png'} alt="Arrow" className="absolute top-0 bottom-0 lg:bottom-0 lg:top-[-3rem] w-[6rem] lg:w-[10rem] right-0 lg:right-[-3rem] z-10" />
-            <motion.div
-              viewport={{ once: true }}
-              initial={{ opacity: 0, y: -20 }}
-              whileInView={{ opacity: 1, y: 0 }}
-              transition={{ delay: 0.3, duration: 0.6 }}
-              className="bg-[#352925] p-4 xl:p-6 rounded-xl relative ">
-              <p className="text-white text-lg lg:text-2xl tracking-wide leading-snug pr-[5rem]">Diklaim menjangkau
-                38 provinsi dengan
-                lebih dari  <span className="font-bold">2 juta penerima</span>
-                {' '}per akhir Februari 2025</p>
-            </motion.div>
-          </div>
-        </div>
-      </div>
-      <div className="bg-[#4f2911] pt-16">
-        <div className="flex flex-col max-w-4xl mx-auto relative px-6 pb-10">
-          <motion.div
+    <div ref={ref} className="relative overflow-clip font-sora bg-no-repeat bg-cover bg-top" style={{ backgroundColor: "#e5e5ec" }}>
+      {/* <div className="absolute left-0 w-full bottom-0 h-[20%] z-0"></div> */}
+      <div className="relative z-10 pt-[3rem] xl:pt-[5rem]">
+        <div className="max-w-4xl mx-auto ">
+          <motion.h4 initial={{ opacity: 0, y: -20 }} whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: .5 }}
             viewport={{ once: true }}
-            initial={{ opacity: 0, x: -20 }}
-            whileInView={{ opacity: 1, x: 0 }}
-            transition={{ delay: 0, duration: 0.6 }}
-            className="absolute px-8 left-0 md:left-[8px] shadow-lg py-4 -top-[6rem] text-2xl font-bold text-white rounded-r-full bg-[#8e4c1f]">Beberapa Polemik MBG</motion.div>
-          <div className="flex flex-col md:flex-row flex-wrap relative z-10">
-            {problems.map((problem, index) => (
-              <motion.div
-                viewport={{ once: true }}
-                initial={{ opacity: 0, y: -20 }}
-                whileInView={{ opacity: 1, y: 0 }}
-                transition={{ delay: index * 0.1, duration: 0.6 }}
-                key={index} className={`flex basis-1/2 flex-row items-start space-x-2 text-white text-xl lg:px-4 py-2 h-fit ${index == problems.length - 2 ? 'lg:-mt-12' : ''}`}>
-                <div className="min-w-4 min-h-4 bg-[#e8b795] rounded-full mt-1.5"></div>
-                <p>{problem}</p>
-              </motion.div>
-            ))}
+            className="font-sora w-full px-4 lg:px-0 text-4xl md:text-5xl xl:text-6xl font-bold !leading-tight tracking-wide text-[#0f36d5] text-start">Pertumbuhan Industri Kendaraan Listrik China Kian Tak Terbendung
+          </motion.h4>
+          <p className="px-4 lg:px-0 text-2xl md:text-3xl xl:text-4xl font-bold font-sora py-2">Dominasi Pasar dan Trajektori Pertumbuhan</p>
+          <p className="px-4 lg:px-0 py-8 text-lg md:text-xl xl:text-2xl font-sora xl:max-w-[80%]">China telah melesat menjadi pemimpin global yang tak terbantahkan di industri kendaraan listrik</p>
+          <div className="w-full relative flex flex-row-reverse">
+            <img src={`${baseUrl}/assets/1.1.png`} className="h-full w-[70%] md:w-[60%] ml-auto z-10 relative" />
+            <div className="absolute bg-[#0f36d5] rounded-tl-[4rem] md:rounded-tl-[5rem] rounded-bl-md md:right-[10%] left-0 top-[25%] p-7 md:p-12">
+              <div className="absolute block md:hidden bg-gradient-to-r from-[#0f0fd5] z-40 w-full left-0 rounded-tl-[4rem] md:rounded-tl-[5rem] top-0 h-full"></div>
+              <div className="w-[80%] xs:w-[50%] sm:w-[40%] font-sora text-white text-xl font-semibold space-y-2 z-40 relative">
+                <div>pada 2024</div>
+                <div className="text-[5rem] xs:text-[6rem] w-[80%] md:w-full leading-[0.9] font-bold text-[#ffe354] relative">27%</div>
+                <p>kendaraan listrik baterai (BEV) saja menguasai pasar otomotif domestik</p>
+              </div>
+            </div>
+          </div>
+          <div className="w-full relative py-16 z-10 xl:z-0">
+            <div className="flex md:flex-row flex-col md:items-start items-end">
+              <div className="flex-1 font-sora text-[#272e3f] text-xl space-y-2 px-6 lg:px-10 py-1">
+                <p>Lebih mengesankan lagi,
+                  jika kita melihat keseluruhan penjualan kendaraan listrik, termasuk hybrid plug-in (PHEV), merek-merek China </p>
+              </div>
+              <div className="flex-1 font-sora text-[#272e3f] text-xl font-semibold px-6 lg:px-0 py-8 md:py-0 text-right md:text-left">
+                <div>Menguasai</div>
+                <p className="text-[5rem] xs:text-[7rem] xl:text-[8rem] text-[#0f36d5] font-bold leading-[0.9]">62%</p>
+                <p>Pangsa Pasar global</p>
+              </div>
+            </div>
           </div>
         </div>
-        <img src={`${baseUrl}/assets/1.6.png`} alt="" className="absolute bottom-0 right-0 w-[50%] lg:w-[45%] z-0" />
+        <div className="relative flex items-center justify-center z-0 xl:z-0">
+          <img src={`${baseUrl}/assets/1.0.png`} className="bottom-0 max-w-4xl -mt-[20rem] xs:-mt-[25rem] sm:-mt-[30rem] xl:-mt-[40rem] h-full w-full mx-auto relative z-10" />
+          <img src={`${baseUrl}/assets/1.0.png`} className="bottom-[-5rem] scale-[1.3] mx-auto absolute z-0 blur-3xl" />
+          <img src={`${baseUrl}/assets/footer.png`} className="z-50 absolute max-w-4xl hidden sm:block bottom-0 w-full mx-auto" />
+        </div>
       </div>
-      <img src={`${baseUrl}/assets/footer.png`} className="z-50 hidden sm:block relative bottom-0 h-full w-fit mx-auto" />
-    </div >
+
+    </div>
   );
 
 }
--- dominasi-mobil-listrik-china-tak-terbendung-amerika-dan-eropa-semakin-sulit-mengejar/src/components/custom-components/CustomImageB.tsx
+++ dominasi-mobil-listrik-china-tak-terbendung-amerika-dan-eropa-semakin-sulit-mengejar/src/components/custom-components/CustomImageB.tsx
@@ -19,140 +19,46 @@ export function CustomImageB() {
   return (
     <div className="relative flex flex-col items-center overflow-hidden ">
       <div
-        className="relative flex h-full w-full justify-center bg-cover bg-center text-center pt-10 xl:pt-32"
+        className="relative flex h-full w-full justify-center bg-cover bg-[center_top_55%] text-center pt-10 xl:pt-32"
         style={{ backgroundImage: `url(${baseUrl}/assets/2.0.jpg)` }}
       >
-        <div className="absolute w-full h-[50%] bg-gradient-to-b from-[#51534d] top-0" />
+        <div className="absolute w-full h-[50%] bg-gradient-to-b from-[#011625] top-0" />
+        <div className="absolute w-full h-[50%] bg-gradient-to-t from-black bottom-0" />
         <div className="z-10 w-full">
-          <div className="max-w-4xl mx-auto relative space-y-2 inset-x-0 z-10 mb-4 text-center font-sora text-3xl font-bold text-white md:text-6xl flex flex-col justify-center items-center">
-            <motion.h3
+          <div className="max-w-4xl mx-auto relative space-y-2 inset-x-0 z-10 mb-4 text-left font-sora text-3xl font-bold text-[#ffe354] md:text-6xl flex flex-col justify-center items-center">
+            <motion.h4
               viewport={{ once: true }}
               initial={{ opacity: 0, y: 20 }}
               whileInView={{ opacity: 1, y: 0 }}
               transition={{ delay: 0, duration: 0.6 }}
-              className="px-4 py-2 rounded-xl break-words w-fit">
-              Program Makan Gratis di Malaysia, Sudah Ada sejak 1979
-            </motion.h3>
+              className="py-2 rounded-xl break-words w-fit !leading-tight px-4 md:px-6 xl:px-0 text-4xl md:text-5xl xl:text-6xl">
+              Dukungan Pemerintah dan Inisiatif Strategis
+            </motion.h4>
           </div>
-          <div className="flex max-w-4xl mx-auto z-10 relative">
-            <div className="basis-2/3" />
-            <div className="basis-1/3">
-              <motion.div
-                viewport={{ once: true }}
-                initial={{ opacity: 0, x: 20 }}
-                whileInView={{ opacity: 1, x: 0 }}
-                transition={{ delay: 0, duration: 0.6 }}
-                className="w-[20rem] bg-blue-500 text-white relative font-sora p-8 rounded-2xl">
-                <motion.img
-                  viewport={{ once: true }}
-                  initial={{ opacity: 0, scale: 0 }}
-                  whileInView={{ opacity: 1, scale: 1 }}
-                  transition={{ delay: 0.1, duration: 0.5 }}
-                  src={baseUrl + "/assets/2.1.png"} alt="Plate" className="absolute top-[-2rem] right-[-2rem] w-[10rem] z-10" />
-
-                <div className="space-y-4 text-left">
-                  <h6 className="pb-1 text-2xl font-semibold  mr-[4rem]">Rancangan
-                    Makanan
-                    Tambahan
-                    (RMT)</h6>
-                  <p className="text-lg">
-                    Dimulai pada 1979,
-                    Pemerintah Malaysia membagikan makanan dan susu selama {" "}<b>190 hari</b> dalam setahun
-                  </p>
-                  <p className="text-lg">Didanai Kementerian Pendidikan. Untuk 2025, anggarannya{" "}
-                    <b>869 juta ringgit</b>
-                  </p>
-                </div>
-
-              </motion.div>
-              <motion.div
-                viewport={{ once: true }}
-                initial={{ opacity: 0, x: 20 }}
-                whileInView={{ opacity: 1, x: 0 }}
-                transition={{ delay: 0.2, duration: 0.4 }}
-                className="bg-black w-[20rem] text-white relative font-sora p-8 rounded-2xl -mt-6">
-                <motion.img
-                  viewport={{ once: true }}
-                  initial={{ opacity: 0, x: 20 }}
-                  whileInView={{ opacity: 1, x: 0 }}
-                  transition={{ delay: 0.3, duration: 0.6 }}
-                  src={baseUrl + "/assets/2.2.png"} alt="Plate" className="absolute top-[-2rem] right-0 w-[10rem] z-10" />
-                <div className="text-lg text-left relative z-10">
-                  <div>Penerima:</div>
-                  <div className="mr-[5rem] pb-1.5">Siswa yang
-                    memenuhi
-                    salah satu dari
-                    kriteria berikut:</div>
-                  <ol className="pl-5 list-decimal font-bold">
-                    <li>Pendapatan kepala
-                      keluarganya di bawah standar kemiskinan
-                    </li>
-                    <li>Orang Asli dan
-                      Orang Penan</li>
-                    <li>Berkebutuhan khusus</li>
-                  </ol>
-                </div>
-              </motion.div>
+          <div className="flex flex-col sm:flex-row items-end sm:items-center max-w-4xl mx-auto relative">
+            <div className="flex-1 font-sora text-lg text-left text-white space-y-4 px-4 md:px-6 xl:px-0">
+              <div>Kesuksesan luar biasa industri EV China tak lepas dari dukungan pemerintah yang besar dan konsisten</div>
+              <div>Beijing telah menggelontorkan dana sekitar <span className="text-[#ffe354] font-bold">US$230</span> miliar untuk sektor ini antara 2009 dan 2023.</div>
+            </div>
+            <div className="flex-1 pr-4 -mb-[2rem]">
+              <img src={`${baseUrl}/assets/2.1.png`} alt="Presiden Cina" />
             </div>
           </div>
-          <div className="bg-[#272727] h-full w-full -mt-5 pt-20 font-sora relative">
-            <div className="absolute top-[0rem] sm:top-[-4rem] lg:top-[-10rem] left-0 right-[-2rem] w-full">
-              <motion.img
-                viewport={{ once: true }}
-                initial={{ opacity: 0 }}
-                whileInView={{ opacity: 1 }}
-                transition={{ delay: 0, duration: 0.6 }}
-                src={baseUrl + '/assets/2.4.png'} className="object-contain w-full scale-[2] md:scale-100" alt="Flag" />
-            </div>
-            <div className="flex max-w-4xl mx-auto w-full flex-col lg:flex-row relative z-10 bg-[#272727]">
-              <div className="basis-1/2 flex flex-col p-8 items-start justify-start">
-                <motion.h4
-                  viewport={{ once: true }}
-                  initial={{ opacity: 0, y: -20 }}
-                  whileInView={{ opacity: 1, y: 0 }}
-                  transition={{ delay: 0, duration: 0.6 }}
-                  className="text-3xl text-white text-left w-full pb-6 font-semibold">Hidangan Berkhasiat di Sekolah (HiTS)</motion.h4>
-                <ul className="text-left text-white text-lg space-y-2">
-                  <motion.li
-                    viewport={{ once: true }}
-                    initial={{ opacity: 0, y: -20 }}
-                    whileInView={{ opacity: 1, y: 0 }}
-                    transition={{ delay: 0, duration: 0.6 }}
-                    className="flex space-x-2 items-start">
-                    <div className="w-0 h-0 border-t-[10px] mt-1.5 scale-[0.80] border-t-transparent border-b-[10px] border-b-transparent border-l-[20px] border-l-yellow-500"></div>
-                    <p>Dimulai sejak 2007 di Muar, Johor</p>
-                  </motion.li>
-                  <motion.li
-                    viewport={{ once: true }}
-                    initial={{ opacity: 0, y: -20 }}
-                    whileInView={{ opacity: 1, y: 0 }}
-                    transition={{ delay: 0.1, duration: 0.5 }}
-                    className="flex space-x-2 items-start">
-                    <div className="w-0 h-0 border-t-[10px] mt-1.5 scale-[0.80] border-t-transparent border-b-[10px] border-b-transparent border-l-[20px] border-l-yellow-500"></div>
-                    <p>Didanai pihak sekolah dan orang tua siswa</p>
-                  </motion.li>
-                  <motion.li
-                    viewport={{ once: true }}
-                    initial={{ opacity: 0, y: -20 }}
-                    whileInView={{ opacity: 1, y: 0 }}
-                    transition={{ delay: 0.2, duration: 0.4 }}
-                    className="flex space-x-2 items-start">
-                    <div className="w-0 h-0 border-t-[10px] mt-1.5 scale-[0.80] border-t-transparent border-b-[10px] border-b-transparent border-l-[20px] border-l-yellow-500"></div>
-                    <p>Tersedia pilihan setidaknya
-                      60 menu, lebih banyak dari
-                      RMT yang hanya 20 menu</p>
-                  </motion.li>
-                </ul>
-              </div>
-              <motion.img
-                viewport={{ once: true }}
-                initial={{ opacity: 0, y: -20 }}
-                whileInView={{ opacity: 1, y: 0 }}
-                transition={{ delay: 0.2, duration: 0.4 }}
-                src={baseUrl + '/assets/2.3.jpg'} alt="Children" className="basis-1/2 w-full lg:w-1/2" />
-            </div>
-            <img src={`${baseUrl}/assets/footer.png`} className="z-50 hidden sm:block relative bottom-0 h-fit w-fit mx-auto" alt="Footer" />
+          <div className="max-w-4xl mx-auto relative scale-125 xs:scale-100">
+            <img src={`${baseUrl}/assets/2.2.png`} alt="Stats" className="-ml-[1rem] xs:ml-0" />
           </div>
+          <div className="h-[40rem] md:h-[20rem]"></div>
+          <div className="flex flex-col sm:flex-row max-w-4xl mx-auto relative items-end space-y-6 sm:space-y-0">
+            <div className="flex-1 px-4 md:px-12">
+              <p className="text-white text-xl tracking-wider font-medium text-left">BYD, salah satu produsen EV terkemuka di China, menjadi contoh nyata penerima manfaat  dari dukungan besar ini, menerima subsidi pemerintah hingga
+                <span className="font-black text-[#ffe354]">{" "}US$3,7</span> miliar antara 2018 dan 2022.</p>
+            </div>
+            <div className="flex-1 px-4 md:px-12">
+              <img src={`${baseUrl}/assets/2.3.png`} alt="Factory" className="w-full" />
+              <p className="bg-[#98a2b6] p-4 text-neutral-800 text-xl tracking-wider font-medium text-left">Suntikan dana ini memungkinkan BYD dan perusahaan lain untuk berinvestasi dalam riset dan pengembangan, meningkatkan kapasitas produksi, dan menawarkan harga yang kompetitif.</p>
+            </div>
+          </div>
+          <img src={`${baseUrl}/assets/footer.png`} className="z-50 hidden sm:block relative bottom-0 h-fit w-fit mx-auto" alt="Footer" />
         </div>
       </div>
     </div>
