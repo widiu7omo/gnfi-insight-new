8b57d18018cb66f6a5de6eddd304b485f3dc20ad
Danar Widi <widiu7omo@gmail.com>
2025-04-17 21:34:27 +0800

    animation

--- dominasi-mobil-listrik-china-tak-terbendung-amerika-dan-eropa-semakin-sulit-mengejar/src/components/custom-components/CustomImageB.tsx
+++ dominasi-mobil-listrik-china-tak-terbendung-amerika-dan-eropa-semakin-sulit-mengejar/src/components/custom-components/CustomImageB.tsx
@@ -37,26 +37,56 @@ export function CustomImageB() {
           </div>
           <div className="flex flex-col sm:flex-row items-end sm:items-center max-w-4xl mx-auto relative">
             <div className="flex-1 font-sora text-lg text-left text-white space-y-4 px-4 md:px-6 xl:px-0">
-              <div>Kesuksesan luar biasa industri EV China tak lepas dari dukungan pemerintah yang besar dan konsisten</div>
-              <div>Beijing telah menggelontorkan dana sekitar <span className="text-[#ffe354] font-bold">US$230</span> miliar untuk sektor ini antara 2009 dan 2023.</div>
+              <motion.div
+                initial={{ opacity: 0 }}
+                whileInView={{ opacity: 1 }}
+                transition={{ duration: .5, delay: .1 }}
+                viewport={{ once: true }}
+              >Kesuksesan luar biasa industri EV China tak lepas dari dukungan pemerintah yang besar dan konsisten</motion.div>
+              <motion.div
+                initial={{ opacity: 0 }}
+                whileInView={{ opacity: 1 }}
+                transition={{ duration: .5, delay: .2 }}
+                viewport={{ once: true }}
+              >Beijing telah menggelontorkan dana sekitar <span className="text-[#ffe354] font-bold">US$230</span> miliar untuk sektor ini antara 2009 dan 2023.</motion.div>
             </div>
             <div className="flex-1 pr-4 -mb-[2rem]">
-              <img src={`${baseUrl}/assets/2.1.png`} alt="Presiden Cina" />
+              <motion.img
+                initial={{ opacity: 0, y: '10%' }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: .5, delay: .2 }}
+                viewport={{ once: true }}
+                src={`${baseUrl}/assets/2.1.png`} alt="Presiden Cina" />
             </div>
           </div>
           <div className="max-w-4xl mx-auto relative scale-125 xs:scale-100">
-            <img src={`${baseUrl}/assets/2.2.png`} alt="Stats" className="-ml-[1rem] xs:ml-0" />
+            <motion.img
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1 }}
+              transition={{ duration: .3 }}
+              viewport={{ once: true }}
+              src={`${baseUrl}/assets/2.2.png`} alt="Stats" className="-ml-[1rem] xs:ml-0" />
           </div>
           <div className="h-[40rem] md:h-[20rem]"></div>
           <div className="flex flex-col sm:flex-row max-w-4xl mx-auto relative items-end space-y-6 sm:space-y-0">
-            <div className="flex-1 px-4 md:px-12">
+            <motion.div
+              initial={{ opacity: 0, x: '-10%' }}
+              whileInView={{ opacity: 1, x: 0 }}
+              transition={{ duration: .5, delay: .2 }}
+              viewport={{ once: true }}
+              className="flex-1 px-4 md:px-12">
               <p className="text-white text-xl tracking-wider font-medium text-left">BYD, salah satu produsen EV terkemuka di China, menjadi contoh nyata penerima manfaat  dari dukungan besar ini, menerima subsidi pemerintah hingga
                 <span className="font-black text-[#ffe354]">{" "}US$3,7</span> miliar antara 2018 dan 2022.</p>
-            </div>
-            <div className="flex-1 px-4 md:px-12">
+            </motion.div>
+            <motion.div
+              initial={{ opacity: 0, x: '10%' }}
+              whileInView={{ opacity: 1, x: 0 }}
+              transition={{ duration: .5, delay: .2 }}
+              viewport={{ once: true }}
+              className="flex-1 px-4 md:px-12">
               <img src={`${baseUrl}/assets/2.3.png`} alt="Factory" className="w-full" />
               <p className="bg-[#98a2b6] p-4 text-neutral-800 text-xl tracking-wider font-medium text-left">Suntikan dana ini memungkinkan BYD dan perusahaan lain untuk berinvestasi dalam riset dan pengembangan, meningkatkan kapasitas produksi, dan menawarkan harga yang kompetitif.</p>
-            </div>
+            </motion.div>
           </div>
           <img src={`${baseUrl}/assets/footer.png`} className="z-50 hidden sm:block relative bottom-0 h-fit w-fit mx-auto" alt="Footer" />
         </div>
